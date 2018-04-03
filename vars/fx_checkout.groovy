6e91140debf24d617f66dbd6b68997070105717a
Zopanix <zopanix@gmail.com>
2018-04-03 09:09:16 -0400

    Added fx_checkout.groovy

---
+++ vars/fx_checkout.groovy
@@ -0,0 +1,11 @@
+def call(scm){
+  checkout scm
+  def scmInfo.commitId = command('git rev-parse HEAD').trim()
+  def scmInfo.branch   = command('echo "${BRANCH_NAME}"').trim()
+  try{
+    def scmInfo.tag = command('git describe --tags --exact-match').trim()
+  }catch(error){
+    def scmInfo.tag = ''
+  }
+  return scmInfo
+}
