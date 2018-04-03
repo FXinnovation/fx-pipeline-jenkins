689653bf03b0b54232e3fb69a3a13df1abfc0ea1
Zopanix <zopanix@gmail.com>
2018-04-03 12:47:18 -0400

    Trying to checkout

--- vars/fx_checkout.groovy
+++ vars/fx_checkout.groovy
@@ -1,11 +1,12 @@
 def call(){
   checkout scm
-  def scmInfo.commitId = command('git rev-parse HEAD').trim()
-  def scmInfo.branch   = command('echo "${BRANCH_NAME}"').trim()
+  def scmInfo = {}
+  scmInfo.commitId = command('git rev-parse HEAD').trim()
+  scmInfo.branch   = command('echo "${BRANCH_NAME}"').trim()
   try{
-    def scmInfo.tag = command('git describe --tags --exact-match').trim()
+    scmInfo.tag = command('git describe --tags --exact-match').trim()
   }catch(error){
-    def scmInfo.tag = ''
+    scmInfo.tag = ''
   }
   return scmInfo
 }
