47fc1d2961372e5a05ef0a53f21408f9fcba0a16
Zopanix <zopanix@gmail.com>
2018-04-18 08:42:13 -0400

    Making final changes to stove library

--- vars/stove.groovy
+++ vars/stove.groovy
@@ -16,34 +16,30 @@ def call(Map config = [:]){
   def keyPath       = ''
   def output        = ''
 
-  println config.dockerImage
-  println config.options
-  println config.tag
-  println config.credentialId
-  // try{
-  //   withCredentials([
-  //     sshUserPrivateKey(
-  //       credentialId:       config.credentialId,
-  //       keyFileVariable:    stoveKey,
-  //       passphraseVariable: stovePassphrase,
-  //       usernameVariable:   stoveUsername
-  //     )
-  //   ]){
-  //     try{
-  //       sh "docker run --rm ${dockerImage} chef exec stove --version"
-  //       dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${dockerImage} chef exec"
-  //       keyPath = '/secrets/key.pem'
-  //     }catch(err){
-  //       dockerCommand = ''
-  //       keyPath = stoveKey
-  //     }
-  //     if tag != '' {
-  //       sh "cat metadata.rb | grep -E '^version\\s' | grep '${tag}'"
-  //     }
-  //     output = command("${dockerCommand} stove ${options} --username ${stoveUsername} --key ${keyPath} ./").trim()
-  //   }
-  // }catch(error){
-  //   error(output)
-  // }
-  // return output
+  try{
+    withCredentials([
+      sshUserPrivateKey(
+        credentialId:       config.credentialId,
+        keyFileVariable:    stoveKey,
+        passphraseVariable: stovePassphrase,
+        usernameVariable:   stoveUsername
+      )
+    ]){
+      try{
+        sh "docker run --rm ${config.dockerImage} chef exec stove --version"
+        dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${config.dockerImage} chef exec"
+        keyPath = '/secrets/key.pem'
+      }catch(err){
+        dockerCommand = ''
+        keyPath = stoveKey
+      }
+      if tag != '' {
+        sh "cat metadata.rb | grep -E '^version\\s' | grep '${config.tag}'"
+      }
+      output = command("${dockerCommand} stove ${config.options} --username ${stoveUsername} --key ${keyPath} ./").trim()
+    }
+  }catch(error){
+    error(output)
+  }
+  return output
 }
