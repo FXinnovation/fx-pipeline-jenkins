ce2196131aed00f3b09462fd2039d34a9816775a
Zopanix <zopanix@gmail.com>
2018-04-18 08:27:45 -0400

    Debug

--- vars/stove.groovy
+++ vars/stove.groovy
@@ -7,30 +7,34 @@ def call(Map config){
   def keyPath       = ''
   def output        = ''
 
-  try{
-    withCredentials([
-      sshUserPrivateKey(
-        credentialId:       config.credentialId,
-        keyFileVariable:    stoveKey,
-        passphraseVariable: stovePassphrase,
-        usernameVariable:   stoveUsername
-      )
-    ]){
-      try{
-        sh "docker run --rm ${dockerImage} chef exec stove --version"
-        dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${dockerImage} chef exec"
-        keyPath = '/secrets/key.pem'
-      }catch(err){
-        dockerCommand = ''
-        keyPath = stoveKey
-      }
-      if tag != '' {
-        sh "cat metadata.rb | grep -E '^version\\s' | grep '${tag}'"
-      }
-      output = command("${dockerCommand} stove ${options} --username ${stoveUsername} --key ${keyPath} ./").trim()
-    }
-  }catch(error){
-    error(output)
-  }
-  return output
+  println dockerImage
+  println options
+  println tag
+  println config.credentialId
+  // try{
+  //   withCredentials([
+  //     sshUserPrivateKey(
+  //       credentialId:       config.credentialId,
+  //       keyFileVariable:    stoveKey,
+  //       passphraseVariable: stovePassphrase,
+  //       usernameVariable:   stoveUsername
+  //     )
+  //   ]){
+  //     try{
+  //       sh "docker run --rm ${dockerImage} chef exec stove --version"
+  //       dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${dockerImage} chef exec"
+  //       keyPath = '/secrets/key.pem'
+  //     }catch(err){
+  //       dockerCommand = ''
+  //       keyPath = stoveKey
+  //     }
+  //     if tag != '' {
+  //       sh "cat metadata.rb | grep -E '^version\\s' | grep '${tag}'"
+  //     }
+  //     output = command("${dockerCommand} stove ${options} --username ${stoveUsername} --key ${keyPath} ./").trim()
+  //   }
+  // }catch(error){
+  //   error(output)
+  // }
+  // return output
 }
