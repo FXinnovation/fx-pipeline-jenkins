15c87b88453dfb8f33439c665e9201cf66a86819
Zopanix <zopanix@gmail.com>
2018-04-18 09:07:14 -0400

    Making some breaking changes

--- vars/stove.groovy
+++ vars/stove.groovy
@@ -8,8 +8,8 @@ def call(Map config = [:]){
   if ( !config.containsKey('tag') ){
     config.tag = ''
   }
-  if ( !config.containsKey('credentialId') ){
-    error('stove.groovy - The credentialId parameter must be set')
+  if ( !config.containsKey('credentialsId') ){
+    error('stove.groovy - The credentialsId parameter must be set')
   }
 
   def dockerCommand = ''
@@ -19,29 +19,27 @@ def call(Map config = [:]){
   def stovePassphrase
   def stoveUsername
 
-  println config.credentialId
-
   try{
     withCredentials([
       sshUserPrivateKey(
-        credentialId:       "${config.credentialId}",
+        credentialsId:      config.credentialsId,
         keyFileVariable:    stoveKey,
         passphraseVariable: stovePassphrase,
         usernameVariable:   stoveUsername
       )
     ]){
-//      try{
-//        sh "docker run --rm ${config.dockerImage} chef exec stove --version"
-//        dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${config.dockerImage} chef exec"
-//        keyPath = '/secrets/key.pem'
-//      }catch(err){
-//        dockerCommand = ''
-//        keyPath = stoveKey
-//      }
-//      if tag != '' {
-//        sh "cat metadata.rb | grep -E '^version\\s' | grep '${config.tag}'"
-//      }
-//      output = command("${dockerCommand} stove ${config.options} --username ${stoveUsername} --key ${keyPath} ./").trim()
+      try{
+        sh "docker run --rm ${config.dockerImage} chef exec stove --version"
+        dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${config.dockerImage} chef exec"
+        keyPath = '/secrets/key.pem'
+      }catch(err){
+        dockerCommand = ''
+        keyPath = stoveKey
+      }
+      if tag != '' {
+        sh "cat metadata.rb | grep -E '^version\\s' | grep '${config.tag}'"
+      }
+      output = command("${dockerCommand} stove ${config.options} --username ${stoveUsername} --key ${keyPath} ./").trim()
     }
   }catch(error){
     throw error
