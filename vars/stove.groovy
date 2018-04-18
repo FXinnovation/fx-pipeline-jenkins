92e7d1b72819163bee0ff2726ccfe17653af8b0e
Zopanix <zopanix@gmail.com>
2018-04-18 08:47:48 -0400

    Debugging

--- vars/stove.groovy
+++ vars/stove.groovy
@@ -19,24 +19,24 @@ def call(Map config = [:]){
   try{
     withCredentials([
       sshUserPrivateKey(
-        credentialId:       'foo',
+        credentialId:       config.credentialId,
         keyFileVariable:    stoveKey,
         passphraseVariable: stovePassphrase,
         usernameVariable:   stoveUsername
       )
     ]){
-      try{
-        sh "docker run --rm ${config.dockerImage} chef exec stove --version"
-        dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${config.dockerImage} chef exec"
-        keyPath = '/secrets/key.pem'
-      }catch(err){
-        dockerCommand = ''
-        keyPath = stoveKey
-      }
-      if tag != '' {
-        sh "cat metadata.rb | grep -E '^version\\s' | grep '${config.tag}'"
-      }
-      output = command("${dockerCommand} stove ${config.options} --username ${stoveUsername} --key ${keyPath} ./").trim()
+//      try{
+//        sh "docker run --rm ${config.dockerImage} chef exec stove --version"
+//        dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${config.dockerImage} chef exec"
+//        keyPath = '/secrets/key.pem'
+//      }catch(err){
+//        dockerCommand = ''
+//        keyPath = stoveKey
+//      }
+//      if tag != '' {
+//        sh "cat metadata.rb | grep -E '^version\\s' | grep '${config.tag}'"
+//      }
+//      output = command("${dockerCommand} stove ${config.options} --username ${stoveUsername} --key ${keyPath} ./").trim()
     }
   }catch(error){
     error(output)
