cb24fb90b49746dd0141531ea9837dbc11797ec1
Zopanix <zopanix@gmail.com>
2018-04-18 07:07:52 -0400

    Simplify stove step

--- vars/stove.groovy
+++ vars/stove.groovy
@@ -1,6 +1,4 @@
-def call(config = [:]){
-  def credentialId = config.credentialId
-  def debug        = config.debug ?: false
+def call(Map config){
   def dockerImage  = config.dockerImage ?: 'fxinnovation/chefdk:latest'
   def options      = config.options ?: '--no-git'
   def tag          = config.tag ?: ''
@@ -10,61 +8,29 @@ def call(config = [:]){
   def output        = ''
 
   try{
-    log(
-      message: 'Loading credentials',
-      output:  debug
-    )
     withCredentials([
       sshUserPrivateKey(
-        credentialId:       credentialId,
+        credentialId:       config.credentialId,
         keyFileVariable:    stoveKey,
         passphraseVariable: stovePassphrase,
         usernameVariable:   stoveUsername
       )
     ]){
-      log(
-        message: 'Checking if docker is available',
-        output:  debug
-      )
       try{
         sh "docker run --rm ${dockerImage} chef exec stove --version"
         dockerCommand = "docker run --rm -v \$(pwd):/data -v ${stoveKey}:/secrets/key.pem ${dockerImage} chef exec"
         keyPath = '/secrets/key.pem'
-        log(
-          message: 'Launching stove using docker',
-          output:  debug
-        )
       }catch(error){
         dockerCommand = ''
         keyPath = stoveKey
-        log(
-          message: 'Launching stove using native stove (must be preinstalled)',
-          output:  debug
-        )
       }
-      log(
-        message: 'Verifying if tag that was passed matches metadata.rb tag',
-        output:  debug
-      )
       if tag != '' {
         sh "cat metadata.rb | grep -E '^version\\s' | grep '${tag}'"
       }
-      log(
-        message: 'Launching stove',
-        output:  debug
-      )
       output = command("${dockerCommand} stove ${options} --username ${stoveUsername} --key ${keyPath} ./").trim()
     }
   }catch(error){
-    log(
-      message: 'Stove failed throwing the error',
-      output:  debug
-    )
     error(output)
   }
-  log(
-    message: 'Stove was succesfull, returning output',
-    output:  debug
-  )
   return output
 }
