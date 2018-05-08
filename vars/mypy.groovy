db9fe34cdfa9755bb62b07a465582edc20e0b2f5
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2018-05-08 09:24:31 -0400

    fix: includes docker command inside the --version check command

--- vars/mypy.groovy
+++ vars/mypy.groovy
@@ -1,22 +1,41 @@
 def call(Map config = [:]){
-  File currentScript = new File(getClass().protectionDomain.codeSource.location.path)
+  node {
+    File currentScript = new File(getClass().protectionDomain.codeSource.location.path)
 
-  if ( !config.containsKey('dockerImage') ){
-    config.dockerImage = 'fxinnovation/pythonlinters:latest'
-  }
-  if ( !config.containsKey('options') ){
-    config.options = ''
-  }
-  if ( !config.containsKey('filePattern') ){
-    error(currentScript.getName() + ' - filePattern parameter is mandatory')
-  }
-  
-  def output = ''
-  def dockerCommand = 'mypy'
-  try{
-    sh "docker run --rm ${config.dockerImage} --version"
-    dockerCommand = "docker run --rm -v \$(pwd):/data -w /data ${config.dockerImage} mypy"
-  }catch(error){}
-    output = command("${dockerCommand} ${config.options} ${config.filePattern}").trim()
+    if (!config.containsKey('dockerImage')) {
+      config.dockerImage = 'fxinnovation/pythonlinters:latest'
+    }
+    if (!config.containsKey('options')) {
+      config.options = ''
+    }
+    if (!config.containsKey('filePattern')) {
+      error(currentScript.getName() + ' - filePattern parameter is mandatory')
+    }
+
+    def output = ''
+    def dockerCommand = 'mypy'
+    def testCommand = ''
+    try {
+      println "docker run --rm ${config.dockerImage} ${dockerCommand} --version"
+      sh "docker run --rm ${config.dockerImage} ${dockerCommand} --version"
+      testCommand = "docker run --rm -v \$(pwd):/data -w /data ${config.dockerImage} ${dockerCommand}"
+    } catch (error) {
+      println error
+      try {
+        println "python3 -m pip install ${dockerCommand}"
+        //sh "python3 -m pip install ${dockerCommand}"
+        output1 = command("python3 -m pip install ${dockerCommand}").trim()
+        println ${output1}
+        println "${dockerCommand} --version"
+        sh "${dockerCommand} --version"
+        testCommand = "${dockerCommand}"
+      } catch (errorpip) {
+        println errorpip
+      }
+    }
+
+    println "${testCommand} ${config.options} ${config.filePattern}"
+    output = command("${testCommand} ${config.options} ${config.filePattern}").trim()
     return output
+  }
 }
