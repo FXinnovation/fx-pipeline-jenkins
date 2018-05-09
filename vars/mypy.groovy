c6e7673b39407cdcc49e9736eaccb804d0d8a49f
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2018-05-09 15:55:44 -0400

    test: changes to make the build work

--- vars/mypy.groovy
+++ vars/mypy.groovy
@@ -1,41 +1,36 @@
 def call(Map config = [:]){
-  node {
-    File currentScript = new File(getClass().protectionDomain.codeSource.location.path)
+  File currentScript = new File(getClass().protectionDomain.codeSource.location.path)
 
-    if (!config.containsKey('dockerImage')) {
-      config.dockerImage = 'fxinnovation/pythonlinters:latest'
-    }
-    if (!config.containsKey('options')) {
-      config.options = ''
-    }
-    if (!config.containsKey('filePattern')) {
-      error(currentScript.getName() + ' - filePattern parameter is mandatory')
-    }
-
-    def output = ''
-    def dockerCommand = 'mypy'
-    def testCommand = ''
-    try {
-      println "docker run --rm ${config.dockerImage} ${dockerCommand} --version"
-      sh "docker run --rm ${config.dockerImage} ${dockerCommand} --version"
-      testCommand = "docker run --rm -v \$(pwd):/data -w /data ${config.dockerImage} ${dockerCommand}"
-    } catch (error) {
-      println error
-      try {
-        println "python3 -m pip install ${dockerCommand}"
-        //sh "python3 -m pip install ${dockerCommand}"
-        output1 = command("python3 -m pip install ${dockerCommand}").trim()
-        println ${output1}
-        println "${dockerCommand} --version"
-        sh "${dockerCommand} --version"
-        testCommand = "${dockerCommand}"
-      } catch (errorpip) {
-        println errorpip
-      }
-    }
-
-    println "${testCommand} ${config.options} ${config.filePattern}"
-    output = command("${testCommand} ${config.options} ${config.filePattern}").trim()
-    return output
+  if (!config.containsKey('dockerImage')) {
+    config.dockerImage = 'fxinnovation/pythonlinters:latest'
   }
+  if (!config.containsKey('options')) {
+    config.options = ''
+  }
+  if (!config.containsKey('filePattern')) {
+    error(currentScript.getName() + ' - filePattern parameter is mandatory')
+  }
+
+  def output = ''
+  def dockerCommand = 'mypy'
+  def testCommand = ''
+  try {
+    println "docker run --rm ${config.dockerImage} ${dockerCommand} --version"
+    sh "docker run --rm ${config.dockerImage} ${dockerCommand} --version"
+    testCommand = "docker run --rm -v \$(pwd):/data -w /data ${config.dockerImage} ${dockerCommand}"
+  } catch (error) {
+    try {
+      println "python3 -m pip install --user ${dockerCommand}"
+      sh("python3 -m pip install --user ${dockerCommand}")
+      testCommand = "~/.local/bin/${dockerCommand}"
+      sh "${testCommand} --version"
+    } catch (errorpip) {
+      println error
+      println errorpip
+    }
+  }
+
+  println "${testCommand} ${config.options} ${config.filePattern}"
+  output = command("${testCommand} ${config.options} ${config.filePattern}").trim()
+  return output
 }
