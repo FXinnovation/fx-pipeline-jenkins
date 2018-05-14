29203b8cafdc605285e87d02a3b9f98f0f7922f5
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2018-05-14 11:50:37 -0400

    test: changes to make CI works

--- vars/pylint.groovy
+++ vars/pylint.groovy
@@ -1,28 +1,43 @@
 def call(Map config = [:]){
-  File currentScript = new File(getClass().protectionDomain.codeSource.location.path)
+    File currentScript = new File(getClass().protectionDomain.codeSource.location.path)
 
-  if ( !config.containsKey('dockerImage') ){
-    config.dockerImage = 'fxinnovation/pythonlinters:latest'
-  }
-  if ( !config.containsKey('options') ){
-    config.options = ''
-  }
-  if ( !config.containsKey('filePattern') ){
-    error(currentScript.getName() + ' - filePattern parameter is mandatory')
-  }
-  if ( !config.containsKey('pylintRepository') ){
-    config.pylintRepository = 'git@bitbucket.org:fxadmin/public-common-configuration-linters.git'
-  }
+    if ( !config.containsKey('dockerImage') ){
+        config.dockerImage = 'fxinnovation/pythonlinters:latest'
+    }
+    if ( !config.containsKey('options') ){
+        config.options = ''
+    }
+    if ( !config.containsKey('filePattern') ){
+        error(currentScript.getName() + ' - filePattern parameter is mandatory')
+    }
+    if ( !config.containsKey('pylintRepository') ){
+        config.pylintRepository = 'https://bitbucket.org/fxadmin/public-common-configuration-linters.git'
+    }
 
-  sh "git clone ${config.pylintRepository} pylint"
-  sh "cp pylint/.pylintrc ."
+    dir('pylint') {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'temp_bitbucket_credentials', url: "${config.pylintRepository}"]]])
+    }
 
-  def output = ''
-  def dockerCommand = 'pylint'
-  try{
-    sh "docker run --rm ${config.dockerImage} --version"
-    dockerCommand = "docker run --rm -v \$(pwd):/data -w /data ${config.dockerImage} pylint"
-  }catch(error){}
-    output = command("${dockerCommand} ${config.options} ${config.filePattern}").trim()
+    def output = ''
+    def dockerCommand = 'pylint'
+    def configurationOption = '--rcfile pylint/.pylintrc'
+    def testCommand = ''
+    try{
+        sh "docker run --rm ${config.dockerImage} --version"
+        testCommand = "docker run --rm -v \$(pwd):/data -w /data ${config.dockerImage} ${dockerCommand} ${configurationOption}"
+    } catch (error) {
+        try {
+            println "python3 -m pip install --user ${dockerCommand}"
+            sh("python3 -m pip install --user ${dockerCommand}")
+            testCommand = "~/.local/bin/${dockerCommand} ${configurationOption}"
+            sh "${testCommand} --version"
+        } catch (errorpip) {
+            println error
+            println errorpip
+        }
+    }
+
+    println "${testCommand} ${config.options} ${config.filePattern}"
+    output = command("${testCommand} ${config.options} ${config.filePattern}").trim()
     return output
 }
