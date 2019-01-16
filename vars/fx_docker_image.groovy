f8b109a20c0983339039e9f8cf9c4189e49dea95
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-01-16 09:27:27 -0500

    Added wrapping in node()

--- vars/fx_docker_image.groovy
+++ vars/fx_docker_image.groovy
@@ -12,38 +12,40 @@ def call (Map config = [:]){
       $class: 'PeriodicFolderTrigger',
       interval: '1d']])
   ])
-  try{
-    result = "SUCCESS"
-    if (!config.containsKey('imageName')){
-      error('The imageName parameter is mandatory.')
-    }
-    stage('pre-build'){
-      scmInfo = fx_checkout()
-      sh 'docker --version && docker images'
-      if (scmInfo.tag == ''){
-        image_tag = scmInfo.commitId
-      }else{
-        image_tag = scmInfo.tag
+  node(){
+    try{
+      result = "SUCCESS"
+      if (!config.containsKey('imageName')){
+        error('The imageName parameter is mandatory.')
       }
-    }
-    stage('build'){
-      sh "docker build \
-             --build-arg \"VCS_REF\"=\"${scmInfo.commitId}\" \
-             --build-arg \"VERSION\"=\"${image_tag}\" \
-             --build-arg \"BUILD_DATE\"=\"\$(date -u +\"%Y-%m-%dT%H:%M:%SZ\")\" \
-             -t ${config.imageName}:${image_tag} ."
-      sh "docker inspect ${config.imageName}:${image_tag}"
-    }
-  }catch(error){
-    result = "FAILURE"
-  }finally{
-    stage('notify'){
-      fx_notify(
-        status: result
-      )
-    }
-    stage('clean'){
-      cleanWS()
+      stage('pre-build'){
+        scmInfo = fx_checkout()
+        sh 'docker --version && docker images'
+        if (scmInfo.tag == ''){
+          image_tag = scmInfo.commitId
+        }else{
+          image_tag = scmInfo.tag
+        }
+      }
+      stage('build'){
+        sh "docker build \
+               --build-arg \"VCS_REF\"=\"${scmInfo.commitId}\" \
+               --build-arg \"VERSION\"=\"${image_tag}\" \
+               --build-arg \"BUILD_DATE\"=\"\$(date -u +\"%Y-%m-%dT%H:%M:%SZ\")\" \
+               -t ${config.imageName}:${image_tag} ."
+        sh "docker inspect ${config.imageName}:${image_tag}"
+      }
+    }catch(error){
+      result = "FAILURE"
+    }finally{
+      stage('notify'){
+        fx_notify(
+          status: result
+        )
+      }
+      stage('clean'){
+        cleanWS()
+      }
     }
   }
 }
