bb01454097fd52c2a3173c964e33d4ce38845a03
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-01-22 11:06:32 -0500

    feat: allows use of multiple command targets

--- vars/fx_terraform.groovy
+++ vars/fx_terraform.groovy
@@ -2,82 +2,21 @@ def call(Map config = [:]) {
   if (!config.containsKey('testEnvironmentCredentialId')) {
     error('“testEnvironmentCredentialId” parameter is mandatory.')
   }
-  if (!config.containsKey('terraformCommandTarget')) {
-    config.terraformCommandTarget = '.'
+  if (!config.containsKey('terraformCommandTargets')) {
+    config.terraformCommandTargets = ['.']
   }
 
   node {
     result="SUCCESS"
     try {
       ansiColor('xterm') {
-        stage('checkout') {
-          scmInfo = fx_checkout()
-        }
+        stageCheckout()
 
-        stage('init') {
-          println terraform.init(
-            commandTarget: config.terraformCommandTarget
-          )
-        }
+        stageInit(config.terraformCommandTargets)
 
-        stage('validate'){
-          println terraform.validate(
-            commandTarget: config.terraformCommandTarget
-          )
-          println terraform.fmt(
-            check: true,
-            commandTarget: config.terraformCommandTarget
-          )
-        }
+        stageValidate(config.terraformCommandTargets)
 
-        withCredentials([
-          usernamePassword(
-            credentialsId: config.testEnvironmentCredentialId,
-            usernameVariable: 'TF_access_key',
-            passwordVariable: 'TF_secret_key'
-          )
-        ]) {
-          try {
-            println plan(
-              TF_access_key,
-              TF_secret_key,
-              config.terraformCommandTarget
-            )
-
-            println terraform.apply(
-              parallelism: 1,
-              refresh: false,
-              commandTarget: 'plan.out'
-            )
-
-            replay = plan(
-              TF_access_key,
-              TF_secret_key,
-              config.terraformCommandTarget
-            )
-
-            println '###############################'
-            println replay
-            println '###############################'
-            if ( !(replay =~ /.*Infrastructure is up-to-date.*/) ){
-              error('Replaying the “apply” contains new changes. Make sure your terraform consecutive run makes no changes.')
-            }
-          } catch (errorApply) {
-            archiveArtifacts(
-              allowEmptyArchive: true,
-              artifacts: 'terraform.tfstat*'
-            )
-            throw (errorApply)
-          } finally {
-            terraform.destroy(
-              vars: [
-                "access_key=${TF_access_key}",
-                "secret_key=${TF_secret_key}"
-              ],
-              commandTarget: config.terraformCommandTarget
-            )
-          }
-        }
+        stageTest(config.terraformCommandTargets, config.testEnvironmentCredentialId)
       }
     }catch (error){
       result='FAILURE'
@@ -93,11 +32,11 @@ def call(Map config = [:]) {
 }
 
 /**
- *Terraform plan & apply
+ * Terraform plan.
  * @param username
  * @param password
  * @param commandTarget
- * @return String terraform apply output
+ * @return String terraform plan output
  */
 def plan(String username, String password, String commandTarget){
   return terraform.plan(
@@ -108,4 +47,103 @@ def plan(String username, String password, String commandTarget){
     ],
     commandTarget: commandTarget
   )
+}
+
+
+/**
+ * Executes checkout stage.
+ */
+def stageCheckout(){
+  stage('checkout') {
+    scmInfo = fx_checkout()
+  }
+}
+
+/**
+ * Executes init stage.
+ * @param commandTargets
+ */
+def stageInit(ArrayList commandTargets){
+  stage('init') {
+    for ( commandTarget in commandTargets ) {
+      println terraform.init(
+        commandTarget: commandTarget
+      )
+    }
+  }
+}
+
+/**
+ * Executes validate stage.
+ * @param commandTargets
+ */
+def stageValidate(ArrayList commandTargets){
+  stage('validate'){
+    for ( commandTarget in commandTargets ) {
+      println terraform.validate(
+        commandTarget: commandTarget
+      )
+      println terraform.fmt(
+        check: true,
+        commandTarget: commandTarget
+      )
+    }
+  }
+}
+
+/**
+ * Executes test stage.
+ * @param commandTargets
+ * @param credentialsId
+ */
+def stageTest(ArrayList commandTargets, String credentialsId){
+  stage('test') {
+    withCredentials([
+      usernamePassword(
+        credentialsId: credentialsId,
+        usernameVariable: 'TF_access_key',
+        passwordVariable: 'TF_secret_key'
+      )
+    ]) {
+      for ( commandTarget in commandTargets ) {
+        try {
+          println plan(
+            TF_access_key,
+            TF_secret_key,
+            commandTarget
+          )
+
+          println terraform.apply(
+            parallelism: 1,
+            refresh: false,
+            commandTarget: 'plan.out'
+          )
+
+          replay = plan(
+            TF_access_key,
+            TF_secret_key,
+            commandTarget
+          )
+
+          if (!(replay =~ /.*Infrastructure is up-to-date.*/)) {
+            error('Replaying the “apply” contains new changes. Make sure your terraform consecutive run makes no changes.')
+          }
+        } catch (errorApply) {
+          archiveArtifacts(
+            allowEmptyArchive: true,
+            artifacts: 'terraform.tfstat*'
+          )
+          throw (errorApply)
+        } finally {
+          terraform.destroy(
+            vars: [
+              "access_key=${TF_access_key}",
+              "secret_key=${TF_secret_key}"
+            ],
+            commandTarget: commandTarget
+          )
+        }
+      }
+    }
+  }
 }
