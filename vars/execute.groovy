a1faa672a0303109b11b2941cb34c6a440067934
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-01-23 14:31:01 -0500

    Adding execute helper

---
+++ vars/execute.groovy
@@ -0,0 +1,36 @@
+def call(Map config = [:]){
+  if (!config.containsKey('script')){
+    error('"script" parameter is mandatory.')
+  } 
+  if (!config.containsKey('returnDetails') || !config.returnDetails instanceof Boolean ){
+    config.containsKey('returnDetails') = true
+  }
+
+  // TODO: Handle windows slaves
+  file_prefix = new Date().getTime()
+  response = [:]
+  sh """
+  set +x
+  touch /tmp/${filePrefix}-all.log
+  tail -f /tmp/${filePrefix}-all.log &
+  TAIL_PID=$!
+  ((${script} | tee /tmp/${filePrefix}-stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/${filePrefix}-stderr.log) &> /tmp/${filePrefix}-all.log
+  echo $? > /tmp/${filePrefix}-statuscode
+  kill \${TAIL_PID}
+  rm /tmp/${filePrefix-all.log}
+  """
+  response.stdout = sh(
+    returnStdout: true,
+    script: "set +x; cat /tmp/${filePrefix}-stdout.log; rm /tmp/${filePrefix}-stdout.log"
+  ).trim()
+  response.stderr = sh(
+    returnStdout: true,
+    script: "set +x; cat /tmp/${filePrefix}-stderr.log; rm /tmp/${filePrefix}-stderr.log"
+  ).trim()
+  response.statusCode = sh(
+    returnStdout: true,
+    script: "set +x; cat /tmp/${filePrefix}-statuscode; rm /tmp/${filePrefix}-statuscode"
+  ).trim().toInteger()
+
+  return response
+}
