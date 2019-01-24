d5e7d195b73fee78a881943ae74613eb93750322
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-01-24 13:01:13 -0500

    More debug

--- vars/execute.groovy
+++ vars/execute.groovy
@@ -23,8 +23,9 @@ def call(Map config = [:]){
        tail -f /tmp/$filePrefix-all.log &
        TAIL_PID=\$!
        ((${config.script} | tee /tmp/${filePrefix}-stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/${filePrefix}-stderr.log) &> /tmp/${filePrefix}-all.log
-       echo \${PIPESTATUS[0]} > /tmp/${filePrefix}-statuscode
-       echo \${PIPESTATUS[@]}
+       foo=\${PIPESTATUS[@]}
+       # echo \${PIPESTATUS[0]} > /tmp/${filePrefix}-statuscode
+       # echo \${PIPESTATUS[@]}
        kill \${TAIL_PID} > /dev/null
        rm /tmp/${filePrefix}-all.log
        """
