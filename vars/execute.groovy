3da9907d0d8e6c31b52dd38d7afa91a138b4c2ba
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-01-24 11:53:05 -0500

    Trying something new

--- vars/execute.groovy
+++ vars/execute.groovy
@@ -16,7 +16,7 @@ def call(Map config = [:]){
        touch /tmp/${filePrefix}-all.log
        tail -f /tmp/$filePrefix-all.log &
        TAIL_PID=\$!
-       ((${config.script}; echo \$? > /tmp/${filePrefix}-statuscode | tee /tmp/${filePrefix}-stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/${filePrefix}-stderr.log) &> /tmp/${filePrefix}-all.log
+       ((${config.script} | tee /tmp/${filePrefix}-stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/${filePrefix}-stderr.log) &> /tmp/${filePrefix}-all.log
        kill \${TAIL_PID}
        rm /tmp/${filePrefix}-all.log
        """
