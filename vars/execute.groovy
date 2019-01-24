92084bbf63abc920c0ffd4314c61b33b3ddd692d
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-01-24 12:38:18 -0500

    Adding debug

--- vars/execute.groovy
+++ vars/execute.groovy
@@ -10,32 +10,32 @@ def call(Map config = [:]){
     statusCode: null
   ]
   println "Executing: '${config.script}'"
-  try {
-    sh """
-       touch /tmp/${filePrefix}-{all,stdout,stderr}.log
-       tail -f /tmp/$filePrefix-all.log &
-       TAIL_PID=\$!
-       ((${config.script} | tee /tmp/${filePrefix}-stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/${filePrefix}-stderr.log) &> /tmp/${filePrefix}-all.log
-       echo "${PIPESTATUS[0]}" > /tmp/${filePrefix}-statuscode
-       kill \${TAIL_PID}
-       rm /tmp/${filePrefix}-all.log
-       """
-  }catch(error){
-    throw error
-  }finally{
-    response.stdout = sh(
-      returnStdout: true,
-      script: "set +e; cat /tmp/${filePrefix}-stdout.log; rm /tmp/${filePrefix}-stdout.log"
-    ).trim()
-    response.stderr = sh(
-      returnStdout: true,
-      script: "set +e; cat /tmp/${filePrefix}-stderr.log; rm /tmp/${filePrefix}-stderr.log"
-    ).trim()
-    response.statusCode = sh(
-      returnStdout: true,
-      script: "set +e; cat /tmp/${filePrefix}-statuscode; rm /tmp/${filePrefix}-statuscode"
-    ).trim().toInteger()
+  sh """
+     touch /tmp/${filePrefix}-all.log
+     touch /tmp/${filePrefix}-stdout.log
+     touch /tmp/${filePrefix}-stderr.log
+     touch /tmp/${filePrefix}-statuscode
 
-    return response
-  }
+     tail -f /tmp/$filePrefix-all.log &
+     TAIL_PID=\$!
+
+     ((${config.script} | tee /tmp/${filePrefix}-stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/${filePrefix}-stderr.log) &> /tmp/${filePrefix}-all.log
+     echo "${PIPESTATUS[0]}" > /tmp/${filePrefix}-statuscode
+     kill \${TAIL_PID}
+     rm /tmp/${filePrefix}-all.log
+     """
+  response.stdout = sh(
+    returnStdout: true,
+    script: "set +e; cat /tmp/${filePrefix}-stdout.log; rm /tmp/${filePrefix}-stdout.log"
+  ).trim()
+  response.stderr = sh(
+    returnStdout: true,
+    script: "set +e; cat /tmp/${filePrefix}-stderr.log; rm /tmp/${filePrefix}-stderr.log"
+  ).trim()
+  response.statusCode = sh(
+    returnStdout: true,
+    script: "set +e; cat /tmp/${filePrefix}-statuscode; rm /tmp/${filePrefix}-statuscode"
+  ).trim().toInteger()
+
+  return response
 }
