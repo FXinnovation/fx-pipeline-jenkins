512a906f140a7c56dd5f9f8b16c4e9ae4480a533
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-02-04 12:03:21 -0500

    Added advance checkout feature

---
+++ vars/advancedCheckout.groovy
@@ -0,0 +1,21 @@
+def call(Map config = [:]){
+  if (config.containsKey('scm') && config.scm instanceof hudson.plugins.git.GitSCM){
+    checkout config.scm
+  }else{
+    if (binding.hasVariable('scm')i && scm instanceof hudson.plugins.git.GitSCM){
+      checkout scm
+    }else{
+      error('advancedCheckout - scm parameter was not given and could not find the scm variable or config.scm or scm was not of class hudson.plugins.git.GitSCM')
+    }
+  }
+  def scmInfo = {}
+  scmInfo.commitId = command('git rev-parse HEAD').trim()
+  scmInfo.branch   = command('echo "${BRANCH_NAME}"').trim()
+  try{
+    scmInfo.tag = command('git describe --tags --exact-match').trim()
+  }catch(error){
+    scmInfo.tag = ''
+  }
+  scmInfo.isPullRequest = scmInfo.branch.matches("^PR-\d$")
+  return scmInfo
+}
