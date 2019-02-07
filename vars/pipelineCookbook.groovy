4082525e57e3329cc527b02542bec713b1671529
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-02-07 10:19:30 -0500

    Added genric cookbook pipeline and adapted public cookbook pipeline

---
+++ vars/pipelineCookbook.groovy
@@ -0,0 +1,57 @@
+def call(Map config = [:], Map closures = [:]){
+  for (closure in closures){
+    if (!closure.value instanceof Closure){
+      error("${closure.key} has to be a Closure")
+    }
+  }
+  if (!closures.containsKey('publish') || !(closures.publish instanceof Closure)){
+    closures.publish = {
+      println "Publishing step was not defined"
+    }
+  }
+  if (!config.containsKey('publish') || !(config.publish instanceof Boolean)){
+    config.publish = false
+  }
+  if (!config.containsKey('cookstyle') || !(config.cookstyle instanceof Map)){
+    config.cookstyle = [:]
+  }
+  if (!config.containsKey('foodcritic') || !(config.foodcritic instanceof Map)){
+    config.foodcritic = [:]
+  }
+  if (!config.containsKey('kitchen') || !(config.kitchen instanceof Map)){
+    config.kitchen = [:]
+  }
+
+  if (closures.containsKey('preTest') || closures.preTest instanceof Closure){
+    stage('pre-test'){
+      closures.preTest()
+    }
+  }
+  stage('test'){
+    cookstyle(config.cookstyle)
+    foodcritic(config.foodcritic)
+    kitchen(config.kitchen)
+  }
+  if (closures.containsKey('postTest') || closures.postTest instanceof Closure){
+    stage('post-test'){
+      closures.postTest()
+    }
+  }
+  if (closures.containsKey('prePublish') || closures.prePublish instanceof Closure){
+    stage('pre-publish'){
+      closures.prePublish()
+    }
+  }
+  stage('publish'){
+    if (config.publish){
+      closures.publish()
+    }else{
+      println "Publish step is skipped"
+    }
+  }
+  if (closures.containsKey('postPublish') || closures.postPublish instanceof Closure){
+    stage('post-publish'){
+      closures.postPublish()
+    }
+  }
+}
--- vars/pipelinePublicCookbook.groovy
+++ vars/pipelinePublicCookbook.groovy
@@ -1,47 +1,16 @@
 def call(Map config = [:], Map closures = [:]){
-  if (!closures.containsKey('preTest') || !(closures.preTest instanceof Closure)){
-    closures.preTest = {}
-  }
-  if (!closures.containsKey('postTest') || !(closures.postTest instanceof Closure)){
-    closures.postTest = {}
-  }
-  if (!closures.containsKey('prePublish') || !(closures.prePublish instanceof Closure)){
-    closures.prePublish = {}
-  }
-  if (!closures.containsKey('postPublish') || !(closures.postPublish instanceof Closure)){
-    closures.postPublish = {}
-  }
-  if (!config.containsKey('publish') || !(config.publish instanceof Boolean)){
-    config.publish = false
-  }
-  if (!config.containsKey('supermarketCredentialsId')){
-    error ('supermarketCredentialsId parameter is mandatory.')
-  }
-
-  stage('pre-test'){
-    closures.preTest()
-  }
-  stage('test'){
-    cookstyle()
-    foodcritic()
-    kitchen()
-  }
-  stage('post-test'){
-    closures.postTest()
-  }
-  stage('pre-publish'){
-    closures.prePublish()
-  }
-  stage('publish'){
-    if (config.publish){
+  if (!closures.containsKey('publish')){
+    if (!config.containsKey('supermarketCredentialsId')){
+      error ('supermarketCredentialsId parameter is mandatory.')
+    }
+    closures.publish = {
       stove(
         credentialsId: config.supermarketCredentialsId
       )
-    }else{
-      println "Publish step is skipped"
     }
   }
-  stage('post-publish'){
-    closures.postPublish()
-  }
+  pipelineCookbook(
+    config,
+    closures
+  )
 }
