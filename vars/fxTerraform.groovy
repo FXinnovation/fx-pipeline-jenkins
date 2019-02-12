8e75e8ffe4455770c6005b563bf5746f698087da
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-02-11 20:53:05 -0500

    fix: somes conditions fixes

--- vars/fxTerraform.groovy
+++ vars/fxTerraform.groovy
@@ -1,13 +1,13 @@
 def call(Map config = [:]) {
 
-  if (!config.containsKey('initSSHCredentialId') || !(config.providerAccessKeyVariableName instanceof CharSequence)) {
+  if (!config.containsKey('initSSHCredentialId') || !(config.initSSHCredentialId instanceof CharSequence)) {
     // default: gitea administrator key
     config.initSSHCredentialId = 'gitea-fx_administrator-key'
   }
   if (!config.containsKey('planVars') || !(config.planVars instanceof List)) {
     config.planVars = []
   }
-  if (!config.containsKey('initSSHHostKeys') || !(config.planVars instanceof List)) {
+  if (!config.containsKey('initSSHHostKeys') || !(config.initSSHHostKeys instanceof List)) {
     // default: gitea host keys
     config.initSSHHostKeys = [
       '|1|V8Cs9iHiY6IHtysrmCUykgmnQEI=|CNbB5msCLZaO4ne4HAdyHNr93eo= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDD4T37qQ3qJENiwfVwog8UfWlMg3Rl6PhEeF3vD1kvSpGjyF88XqgGsdDzTvFYeoc+WhbPQQXqIiowoovh6W4LkNi7SOTf10po0Llxde/xSZc32zQ4fltERf69L2XvQy43a5Apx1GgcLQrbaRj1/zx4Muo3hjvDu/OPkhso6Q734lfgZcy1uFoXaZIadeJOVzQIez3FiAmmr6r48Eb7hntK57u+Xdpd5Fq9zBDoMbzAnsCXZWzYEC/j9Hje+wV5iwyM/UWUaC06zyfG8NvPkwU90mIVxIX6NB9yrGlT0tPuNL69Vz4Ykc9LoHJQoHcetqbzS684vvwDnlXP0TrC/AV',
