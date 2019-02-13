71df7acd46f9e9f9ab1c5d50f24f0dd1e6448868
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-02-12 22:26:46 -0500

    gjhgg

--- vars/pipelineDocker.groovy
+++ vars/pipelineDocker.groovy
@@ -15,6 +15,7 @@ def call(Map config = [:], Map closures =[:]){
     }
   }
   stage('build'){
+    println scmInfo.branch
     dockerImage.build(config.dockerBuild)
   }
   if (closures.containsKey('postBuild') && closures.postBuild instanceof Closure){
