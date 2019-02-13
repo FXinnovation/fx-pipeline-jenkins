71842967ab036f6338c7baca1ebdfe191ae43f70
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-02-12 22:05:12 -0500

    Removing not working code

--- vars/pipelineDocker.groovy
+++ vars/pipelineDocker.groovy
@@ -15,7 +15,6 @@ def call(Map config = [:], Map closures =[:]){
     }
   }
   stage('build'){
-    this.binding.variables.each {k,v -> println "$k = $v"}
     dockerImage.build(config.dockerBuild)
   }
   if (closures.containsKey('postBuild') && closures.postBuild instanceof Closure){
