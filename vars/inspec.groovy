3f85e607d164cfa6c22f4c9b7fe4697810616e50
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-02-15 12:55:20 -0500

    initial inspec helper

---
+++ vars/inspec.groovy
@@ -0,0 +1,36 @@
+def call(Map config = [:]){
+  optionsString = ''
+  if (!config.containsKey('commandTarget') || !(config.commandTarget instanceof CharSequence)){
+    error('commandTarget parameter is mandatory')
+  }
+  if (!config.containsKey('dockerAdditionalMounts') || !(config.dockerAdditionalMounts instanceof CharSequence)){
+    config.dockerAdditionnalMounts = [:]
+  }
+  if (!config.containsKey('dockerEnvironmentVariables') || !(config.dockerEnvironmentVariables instanceof CharSequence)){
+    config.dockerEnvironmentVariables = [:]
+  }
+  if (!config.containsKey('dockerImage') || !(config.dockerImage instanceof CharSequence)){
+    config.dockerImage = 'fxinnovation/inspec:latest'
+  }
+  if (config.containsKey('target') && config.target instanceof CharSequence){
+    optionsString += "--target=${config.target} "
+  }
+  if (!config.containsKey('subCommand') || !(config.subCommand instanceof CharSequence)){
+    error('subCommand parameter is mandatory')
+  }
+
+  inspecCommand = dockerRunCommand(
+    dockerImage: config.dockerImage,
+    fallbackCommand: 'inspec',
+    additionalMounts: config.dockerAdditionalMounts,
+    environmentVariables: config.dockerEnvironmentVariables
+  )
+
+  execute(
+    script: "${inspecCommand} --version"
+  )
+
+  return execute(
+    script: "${inspecCommand} ${config.subCommand} ${optionsString} ${config.commandTarget}"
+  )
+}
