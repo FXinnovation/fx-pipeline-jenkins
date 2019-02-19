40e0a9a0fa95cc0d1ca1c6e413baa460f88054b6
Quentin Vallin <quentin.vallin@fxinnovation.com>
2019-02-19 11:50:23 -0500

    add fxUploadCookbook

---
+++ vars/fxUploadCookbook.groovy
@@ -0,0 +1,35 @@
+def call(Map config = [:]) {
+  if (!(config.credentialId instanceof CharSequence)){
+    error ('credentialId parameter must be of type CharSequence')
+  }
+  if (!(config.serverUrl instanceof CharSequence)) {
+    error ('serverUrl parameter must be of type CharSequence')
+  }
+  if (!(config.cookbookName instanceof CharSequence)) {
+    error ('cookbookName parameter must be of type CharSequence')
+  }
+  if (!(config.publish instanceof Boolean)){
+    error ('publish parameter must be of type Boolean')    
+  }
+
+  if (!config.containsKey('credentialId')) {
+    config.credentialId = 'chef-server-demo'
+  }
+  if(!config.containsKey('serverUrl')) {
+    config.serverUrl = 'https://chef-server.dazzlingwrench.fxinnovation.com/organizations/fx'
+  }
+  if (!config.containsKey('publish')){
+    config.publish = false
+  }
+  
+  fxJob([
+    pipeline: {
+      pipelinePrivateCookbook ([
+        credentialId: config.credentialId,
+        serverUrl: config.serverUrl,
+        cookbookName: config.cookbookName,
+        publish: config.publish,
+        ]),
+    }
+  ])
+}
