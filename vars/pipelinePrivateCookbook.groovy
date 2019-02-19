e2fe718fd04cd3f3e7e7d2043fd3dd822c6931b9
Quentin Vallin <quentin.vallin@fxinnovation.com>
2019-02-19 09:54:10 -0500

    wip 3

--- vars/pipelinePrivateCookbook.groovy
+++ vars/pipelinePrivateCookbook.groovy
@@ -17,7 +17,7 @@ def call(Map config = [:], Map closures = [:]){
         commandTarget: config.cookbookName,
       ) 
       
-      println cookbookUploadOutput.stderr.to_string
+      println cookbookUploadOutput
       if (cookbookUploadOutput.stderr =~ /ERROR: Could not find cookbook/) {
         error(cookbookUploadOutput.stderr)
       } 
