5b5d322da5ca7a07942aa4a219b818fb5a01a454
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-02-19 08:40:53 -0500

    Fix calling knife

--- vars/knife.groovy
+++ vars/knife.groovy
@@ -101,7 +101,7 @@ def call (Map config = [:]){
     )
 
     return execute(
-      script: "${knifeCommand} knife ${config.subCommand} ${optionsString} ${commandTarget}"
+      script: "${knifeCommand} knife ${config.subCommand} ${optionsString} ${config.commandTarget}"
     )
   }
 }
