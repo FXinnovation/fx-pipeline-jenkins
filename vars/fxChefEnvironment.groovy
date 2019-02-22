1d7ccf2c74004d715ecfdc91403f1df5ebe44f9c
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-02-22 09:45:43 -0500

    Fixed three typos

--- vars/fxChefEnvironment.groovy
+++ vars/fxChefEnvironment.groovy
@@ -1,12 +1,12 @@
 def call(Map config = [:]){
   if (!config.containsKey('credentialId') || !(config.credentialId instanceof CharSequence)){
-    config.credentialId = 'chef-server-demo',
+    config.credentialId = 'chef-server-demo'
   }
   if (!config.containsKey('serverUrl') || !(config.serverUrl instanceof CharSequence)){
-    config.serverUrl = 'https://chef-server.dazzlingwrench.fxinnovation.com/organizations/fx',
+    config.serverUrl = 'https://chef-server.dazzlingwrench.fxinnovation.com/organizations/fx'
   }
   if (!config.containsKey('environmentFile') || !(config.environmentFile instanceof CharSequence)){
-    config.environmentFile = 'environment.json',
+    config.environmentFile = 'environment.json'
   }
   fxJob(
     [
@@ -23,7 +23,7 @@ def call(Map config = [:]){
               credentialId: config.credentialId,
               serverUrl: config.serverUrl,
               commandTarget: config.environmentFile
-            ],
+            ]
           ]
         )
       }
