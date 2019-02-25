6cde48ce21b43acd997810190bc1f8f2c815c601
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-02-25 15:59:01 -0500

    test: debugs

--- vars/fxTerraform.groovy
+++ vars/fxTerraform.groovy
@@ -1,23 +1,32 @@
 def call(Map config = [:]) {
   mapAttributeCheck(config, 'initSSHCredentialId', CharSequence, 'gitea-fx_administrator-key')
   mapAttributeCheck(config, 'testPlanVars', List, [])
+  mapAttributeCheck(config, 'publishPlanVars', List, [])
   mapAttributeCheck(config, 'validateVars', List, [])
   mapAttributeCheck(config, 'initSSHHostKeys', List, [
     '|1|V8Cs9iHiY6IHtysrmCUykgmnQEI=|CNbB5msCLZaO4ne4HAdyHNr93eo= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDD4T37qQ3qJENiwfVwog8UfWlMg3Rl6PhEeF3vD1kvSpGjyF88XqgGsdDzTvFYeoc+WhbPQQXqIiowoovh6W4LkNi7SOTf10po0Llxde/xSZc32zQ4fltERf69L2XvQy43a5Apx1GgcLQrbaRj1/zx4Muo3hjvDu/OPkhso6Q734lfgZcy1uFoXaZIadeJOVzQIez3FiAmmr6r48Eb7hntK57u+Xdpd5Fq9zBDoMbzAnsCXZWzYEC/j9Hje+wV5iwyM/UWUaC06zyfG8NvPkwU90mIVxIX6NB9yrGlT0tPuNL69Vz4Ykc9LoHJQoHcetqbzS684vvwDnlXP0TrC/AV',
     '|1|+TYoo5BCu7BRQZl0+TXEK2f6JW8=|zfDEKwHmsNmNGPY1bNj1CNjOPLQ= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAZ3HVe71Jte8O3B6CnnCojmCtQJidELmlSiKbxZphEwnhl6Wr7iF0GH+Oo5k34Q8toPHvmIRPh9UcNTr4g5dHI=',
     '|1|6Kd9WNT+MUC06McJma+j91Fxfik=|aHMHcoEUNdmmgU75HWXfkIGTUWA= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnKb68pJWPerdWlT9m2DraaFalb7K562kUO4SHtpyaL',
   ])
+  mapAttributeCheck(config, 'terraformInitBackendConfigsTest', ArrayList, [])
+  mapAttributeCheck(config, 'terraformInitBackendConfigsPublish', ArrayList, [])
   mapAttributeCheck(config, 'commandTargets', List, ['.'])
 
   fxJob([
     pipeline: { Map scmInfo ->
+      def isTagged = '' != scmInfo.tag
+      def deployFileExists = fileExists 'deploy.tf'
       def publish = false
-      if ( '' != scmInfo.tag ){
+      def manualTrigger = true
+
+      currentBuild.upstreamBuilds?.each { b ->
+        manualTrigger = false
+      }
+      if (isTagged && deployFileExists && manualTrigger){
         publish = true
       }
-
-      println(currentBuild.rawBuild.getCauses())
-
+      env.DEBUG = true
+      printDebug("isTagged: ${isTagged} | deployFileExists: ${deployFileExists} | manualTrigger: ${manualTrigger} | publish:${publish}")
       for (commandTarget in config.commandTargets) {
         pipelineTerraform([
           commandTarget     : commandTarget,
@@ -30,11 +39,21 @@ def call(Map config = [:]) {
           testDestroyOptions: [
             vars: config.testPlanVars
           ],
-          publishPlanOptions: [
-            vars: config.publishPlanVars
+          validateOptions: [
+            vars: config.testPlanVars
           ],
           publish: publish
         ], [
+          preValidate: {
+            if (!publish) {
+              return
+            }
+            for (filename in execute(script: "ls").stdout.split()) {
+              if (filename =~ /.+\.tf$/ && ('deploy.tf' != filename || 'variables.tf' != filename)) {
+                error("The current build is a candidate to publish but it contains a “${filename}” file. This does not comply with FX standard. For deployments, create a single “deploy.tf” with a “variables.tf” file.")
+              }
+            }
+          },
           init: {
             sshagent([config.initSSHCredentialId]) {
               sh('ssh-add -l')
@@ -48,9 +67,47 @@ def call(Map config = [:]) {
                 ],
                 dockerEnvironmentVariables: [
                   'SSH_AUTH_SOCK': '/ssh-agent'
-                ]
+                ],
+                backendConfigs: config.terraformInitBackendConfigsTest
               )
             }
+          },
+          publish: {
+            sshagent([config.initSSHCredentialId]) {
+              sh('ssh-add -l')
+              sh('mkdir -p ~/.ssh')
+              sh('echo "' + config.initSSHHostKeys.join('" >> ~/.ssh/known_hosts && echo "') + '" >> ~/.ssh/known_hosts')
+              terraform.init(
+                commandTarget: commandTarget,
+                dockerAdditionalMounts: [
+                  '~/.ssh/'                       : '/root/.ssh/',
+                  '\$(readlink -f $SSH_AUTH_SOCK)': '/ssh-agent',
+                ],
+                dockerEnvironmentVariables: [
+                  'SSH_AUTH_SOCK': '/ssh-agent'
+                ],
+                backendConfigs: config.terraformInitBackendConfigsPublish
+              )
+            }
+
+            plan = terraform.plan([
+                commandTarget: config.commandTarget,
+              ] + config.publishPlanVars
+            )
+            if (plan.stdout =~ /.*Infrastructure is up-to-date.*/) {
+              error('The “plan” does not contain new changes. Skipping')
+              return
+            }
+
+            fx_notify(
+              status: 'PENDING'
+            )
+            input 'WARNING: You are about to deploy the displayed plan in. Do you want to apply it?'
+            terraform.apply([
+                parallelism: 1,
+                refresh: false,
+              ] + config.testApplyOptions
+            )
           }
         ])
       }
@@ -74,3 +131,13 @@ def call(Map config = [:]) {
     ]
   )
 }
+
+def debug(CharSequence message){
+  // The try/catch with silent catch is to simulate a check “is_variable_defined” that does not exist in groovy
+  try {
+    if (env.DEBUG != null) {
+      println(message)
+    }
+  } catch (MissingPropertyException missingPropertyException) {}
+}
+
