f65dbe100016a28d16282ebc3c6a1d5388613bf2
Quentin Vallin <quentin.vallin@fxinnovation.com>
2019-03-05 08:51:50 -0500

    add publish only on tag

--- vars/fxUploadCookbook.groovy
+++ vars/fxUploadCookbook.groovy
@@ -18,20 +18,22 @@ def call(Map config = [:]) {
   if(!config.containsKey('serverUrl')) {
     config.serverUrl = 'https://chef-server.dazzlingwrench.fxinnovation.com/organizations/fx'
   }
-  if (!config.containsKey('publish')){
-    config.publish = false
-  }
   
   fxJob([
     postPrepare: {
       sh 'ssh-keygen -t rsa -f /tmp/id_rsa -P \'\''
     },
-    pipeline: {
+    pipeline: {Map scmInfo ->  
+      if ( '' != scmInfo.tag ){
+        publish = true
+      }else{
+        publish = false
+      }
       pipelinePrivateCookbook ([
         credentialId: config.credentialId,
         serverUrl: config.serverUrl,
         cookbookName: config.cookbookName,
-        publish: config.publish,
+        publish: publish,
         cookbookPath: "/",
       ])
     }
