9d48940ddaddbe0e6c076f0648360a33941c37bc
Quentin Vallin <quentin.vallin@fxinnovation.com>
2019-03-05 10:21:20 -0500

    fix some bug

--- vars/fxUploadCookbook.groovy
+++ vars/fxUploadCookbook.groovy
@@ -1,15 +1,12 @@
 def call(Map config = [:]) {
-  if (!config.containsKey('credentialId') && !(config.credentialId instanceof CharSequence)){
+  if (config.containsKey('credentialId') && !(config.credentialId instanceof CharSequence)){
     error ('"credentialId" parameter must be of type CharSequence')
   }
-  if (!config.containsKey('serverUrl') && !(config.serverUrl instanceof CharSequence)) {
+  if (config.containsKey('serverUrl') && !(config.serverUrl instanceof CharSequence)) {
     error ('"serverUrl" parameter must be of type CharSequence')
   }
   if (!config.containsKey('cookbookName') && !(config.cookbookName instanceof CharSequence)) {
-    error ('"cookbookName" parameter must be of type CharSequence')
-  }
-  if (!config.containsKey('publish') && !(config.publish instanceof Boolean)){
-    error ('"publish" parameter must be of type Boolean')    
+    error ('"cookbookName" parameter is mandatory and must be of type CharSequence')
   }
 
   if (!config.containsKey('credentialId')) {
