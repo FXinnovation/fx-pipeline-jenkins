d43740a90a7ec54ba811aaea6dc00645969edc2f
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-03-07 11:28:32 -0500

    Separated pipeline in another file

--- vars/fxDockerImage.groovy
+++ vars/fxDockerImage.groovy
@@ -41,67 +41,11 @@ def call(Map config = [:]){
             publish: publish
           ]
         )
-        try{
-          infiniteLoopScript = """
-          while true
-          do
-            sleep 15
-          done
-          """
-          inspecConfig = """
-          {
-            "reporter": {
-              "cli": {
-                "stdout": true
-              },
-              "junit": {
-                "stdout": false,
-                "file": "inspec-results.xml"
-              }
-            }
-          }
-          """
-          writeFile(
-            file: 'inspec-config.json',
-            text: inspecConfig
-          )
-          writeFile(
-            file: 'infiniteLoop.sh',
-            text: infiniteLoopScript
-          )
-          execute(
-            script: "docker run -d \
-              -v \$(pwd):/data \
-              -w /data \
-              --name inspec-test \
-              --entrypoint sh \
-              ${config.namespace}/${config.image}:${tags[0]} \
-              infiniteLoop.sh"
-          )
-          try{
-            inspec.exec(
-              target: 'docker://inspec-test',
-              jsonConfig: 'inspec-config.json',
-              dockerAdditionalMounts: [
-                '/var/run/docker.sock': '/var/run/docker.sock'
-              ],
-              commandTarget: 'https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/inspec-docker-baseline/archive/master.tar.gz'
-            )
-          }catch(uselessError){
-            println 'Inspec tests have failed, but we\'re still being nice for now'
-          }
-        }catch(inspecError){
-          throw (inspecError)
-        }finally{
-          execute(
-            script: 'docker kill inspec-test && docker rm inspec-test',
-            throwError: false
-          )
-          junit(
-            allowEmptyResults: true,
-            testResults: 'inspec-results.xml'
-          )
-        }
+        fxInspecDockerImage(
+          image: config.image,
+          tag: tags[0],
+          namespace: config.namespace
+        )
       }
     ]
   )
---
+++ vars/fxInspecDockerImage.groovy
@@ -0,0 +1,68 @@
+def call(Map config = [:]){
+  if (!config.containsKey('tag') || !config.containsKey('image') || !config.containsKey('namespace')){
+    error('Parameters tag, image and namespace are mandatory.')
+  }
+  stage('inspec'){
+    try{
+      infiniteLoopScript = """
+      while true
+      do
+        sleep 15
+      done
+      """
+      inspecConfig = """
+      {
+        "reporter": {
+          "cli": {
+            "stdout": true
+          },
+          "junit": {
+            "stdout": false,
+            "file": "inspec-results.xml"
+          }
+        }
+      }
+      """
+      writeFile(
+        file: 'inspec-config.json',
+        text: inspecConfig
+      )
+      writeFile(
+        file: 'infiniteLoop.sh',
+        text: infiniteLoopScript
+      )
+      execute(
+        script: "docker run -d \
+          -v \$(pwd):/data \
+          -w /data \
+          --name inspec-test \
+          --entrypoint sh \
+          ${config.namespace}/${config.image}:${config.tag} \
+          infiniteLoop.sh"
+      )
+      try{
+        inspec.exec(
+          target: 'docker://inspec-test',
+          jsonConfig: 'inspec-config.json',
+          dockerAdditionalMounts: [
+            '/var/run/docker.sock': '/var/run/docker.sock'
+          ],
+          commandTarget: 'https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/inspec-docker-baseline/archive/master.tar.gz'
+        )
+      }catch(uselessError){
+        println 'Inspec tests have failed, but we\'re still being nice for now'
+      }
+    }catch(inspecError){
+      throw (inspecError)
+    }finally{
+      execute(
+        script: 'docker kill inspec-test && docker rm inspec-test',
+        throwError: false
+      )
+      junit(
+        allowEmptyResults: true,
+        testResults: 'inspec-results.xml'
+      )
+    }
+  }
+}
