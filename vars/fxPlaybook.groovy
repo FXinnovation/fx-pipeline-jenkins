0f15fef1b975f5f84ac332350da7ce056772817a
Julien Cabillot <julien.cabillot@fxinnovation.com>
2019-03-18 16:39:36 -0400

    Add options

--- vars/fxPlaybook.groovy
+++ vars/fxPlaybook.groovy
@@ -1,13 +1,29 @@
-def call() {
+def call(Map config = [:]) {
+  if (!config.containsKey('ansiblelintConfig')) {
+    config.ansiblelintConfig = [:]
+  } else if (!(config.ansiblelintConfig instanceof Map)) {
+    error('ansiblelintConfig parameter must be of type Map')
+  }
+
+  if (!config.containsKey('ansiblelintOutputFile')) {
+    config.ansiblelintOutputFile = 'ansible-lint.txt'
+  } else if (!(config.ansiblelintOutputFile instanceof CharSequence)) {
+    error('ansiblelintOutputFile parameter must be of type CharSequence')
+  }
+
   fxJob([
     pipeline: {
       try {
-        ansiblelint()
+        ansiblelint(config.ansiblelintConfig)
       } catch(error) {
         writeFile(
-          file: 'ansible-lint.txt',
+          file: config.ansiblelintOutputFile,
           text: error.getMessage()
         )
+        archiveArtifacts(
+          artifacts: config.ansiblelintOutputFile
+        )
+
         throw(error)
       }
     },
