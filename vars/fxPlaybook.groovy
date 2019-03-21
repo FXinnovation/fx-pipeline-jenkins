2d15cc07f9289a5be2df7df3e2c307058b43beb8
Julien Cabillot <julien.cabillot@fxinnovation.com>
2019-03-21 12:54:43 -0400

    Use the new mapAttributeCheck

--- vars/fxPlaybook.groovy
+++ vars/fxPlaybook.groovy
@@ -1,15 +1,6 @@
 def call(Map config = [:]) {
-  if (!config.containsKey('ansiblelintConfig')) {
-    config.ansiblelintConfig = [:]
-  } else if (!(config.ansiblelintConfig instanceof Map)) {
-    error('ansiblelintConfig parameter must be of type Map')
-  }
-
-  if (!config.containsKey('ansiblelintOutputFile')) {
-    config.ansiblelintOutputFile = 'ansible-lint.txt'
-  } else if (!(config.ansiblelintOutputFile instanceof CharSequence)) {
-    error('ansiblelintOutputFile parameter must be of type CharSequence')
-  }
+  mapAttributeCheck(config, 'ansiblelintConfig', Map, [:])
+  mapAttributeCheck(config, 'ansiblelintOutputFile', CharSequence, 'ansible-lint.txt')
 
   fxJob([
     pipeline: {
