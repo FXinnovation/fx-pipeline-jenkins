529c6ad709c3b0ba3a678853bed619d375e7ca9b
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-03-27 12:30:42 -0400

    Removed debug

--- vars/jobInfo.groovy
+++ vars/jobInfo.groovy
@@ -1,6 +1,5 @@
 def isManuallyTriggered(){
   currentBuild.getBuildCauses().each { cause ->
-    println cause
     if (cause.containsKey('_class') && cause['_class'] == 'hudson.model.Cause$UserIdCause') {
       return true
     }
