284f46b0c03d5b4cd3b26a531a1499b42084b52c
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-03-29 16:33:17 -0400

    feat: removes test on publish creds in fxTerraform that makes no sense

--- vars/fxTerraform.groovy
+++ vars/fxTerraform.groovy
@@ -1,8 +1,8 @@
 def call(Map config = [:]) {
-  mapAttributeCheck(config, 'initSSHCredentialId', CharSequence, 'gitea-fx_administrator-key')
   mapAttributeCheck(config, 'testPlanVars', List, [])
-  mapAttributeCheck(config, 'publishPlanVars', List, [])
   mapAttributeCheck(config, 'validateVars', List, [])
+  mapAttributeCheck(config, 'initSSHCredentialId', CharSequence, 'gitea-fx_administrator-key')
+  mapAttributeCheck(config, 'publishPlanVars', List, [])
   mapAttributeCheck(config, 'initSSHHostKeys', List, [
     '|1|V8Cs9iHiY6IHtysrmCUykgmnQEI=|CNbB5msCLZaO4ne4HAdyHNr93eo= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDD4T37qQ3qJENiwfVwog8UfWlMg3Rl6PhEeF3vD1kvSpGjyF88XqgGsdDzTvFYeoc+WhbPQQXqIiowoovh6W4LkNi7SOTf10po0Llxde/xSZc32zQ4fltERf69L2XvQy43a5Apx1GgcLQrbaRj1/zx4Muo3hjvDu/OPkhso6Q734lfgZcy1uFoXaZIadeJOVzQIez3FiAmmr6r48Eb7hntK57u+Xdpd5Fq9zBDoMbzAnsCXZWzYEC/j9Hje+wV5iwyM/UWUaC06zyfG8NvPkwU90mIVxIX6NB9yrGlT0tPuNL69Vz4Ykc9LoHJQoHcetqbzS684vvwDnlXP0TrC/AV',
     '|1|+TYoo5BCu7BRQZl0+TXEK2f6JW8=|zfDEKwHmsNmNGPY1bNj1CNjOPLQ= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAZ3HVe71Jte8O3B6CnnCojmCtQJidELmlSiKbxZphEwnhl6Wr7iF0GH+Oo5k34Q8toPHvmIRPh9UcNTr4g5dHI=',
@@ -65,7 +65,7 @@ def preValidate(Boolean deployFileExists) {
   }
 }
 
-def init(Map config = [:], CharSequence commandTarget) {
+def init(Map config = [:], CharSequence commandTarget, Boolean deployFileExists) {
   sshagent([config.initSSHCredentialId]) {
     sh('ssh-add -l')
     sh('mkdir -p ~/.ssh')
@@ -85,8 +85,6 @@ def init(Map config = [:], CharSequence commandTarget) {
 }
 
 def publish(Map config = [:], CharSequence commandTarget) {
-  mapAttributeCheck(config, 'publishEnvironmentCredentialId', CharSequence, '', '“publishEnvironmentCredentialId” parameter is mandatory.')
-
   plan = terraform.plan(
     commandTarget: commandTarget,
     vars: config.publishPlanVars
