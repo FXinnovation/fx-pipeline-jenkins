579b9b215d002f00d64403f112fea998ab6d9004
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-04-03 11:33:57 -0400

    debug

--- vars/jobInfo.groovy
+++ vars/jobInfo.groovy
@@ -1,8 +1,9 @@
 def isManuallyTriggered(){
   currentBuild.getBuildCauses().each { cause ->
     print(cause)
-    println(cause.containsKey('_class'))
-    println(cause['_class'] == 'hudson.model.Cause$UserIdCause')
+    println('CONTAINS: ' + cause.containsKey('_class'))
+    println('CONTAINS: ' + cause.containsKey('"_class"'))
+    println('EQ : ' + cause['_class'] == 'hudson.model.Cause$UserIdCause')
     if (cause.containsKey('_class') && cause['_class'] == 'hudson.model.Cause$UserIdCause') {
       return true
     }
