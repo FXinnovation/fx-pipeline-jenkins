f08d0bd10bce7ad279e8e4b357a8b3b41983911c
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-04-04 16:46:55 -0400

    Added test results

--- vars/fxNodeJsPipeline.groovy
+++ vars/fxNodeJsPipeline.groovy
@@ -3,6 +3,23 @@ def call(Map config = [:]){
     pipeline: {
       Map scmInfo ->
       pipelineNodeJsYarn()
+    },
+    preNotify: {
+      junit 'xunit.xml'
+      cobertura(
+        autoUpdateHealth: false,
+        autoUpdateStability: false,
+        coberturaReportFile: 'coverage/*',
+        conditionalCoverageTargets: '70, 0, 0',
+        failUnhealthy: false,
+        failUnstable: false,
+        lineCoverageTargets: '80, 0, 0',
+        maxNumberOfBuilds: 0,
+        methodCoverageTargets: '80, 0, 0',
+        onlyStable: false,
+        sourceEncoding: 'ASCII',
+        zoomCoverageChart: false
+      )
     }
   ])
 }
