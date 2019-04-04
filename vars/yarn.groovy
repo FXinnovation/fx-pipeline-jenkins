16b17a1c9da42dc20847afc822b57c03cc86c7c6
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-04-04 14:43:25 -0400

    Added yarn helper

---
+++ vars/yarn.groovy
@@ -0,0 +1,61 @@
+def audit(Map config = [:]){
+  config.subCommand = 'audit'
+  validParameters = [
+    'dockerImage':'',
+    'subCommand':'',
+    'dockerAdditionalMounts':'',
+    'dockerEnvironmentVariables':''
+  ]
+  for ( parameter in config ) {
+    if ( !validParameters.containsKey(parameter.key)){
+      error("yarn - Parameter \"${parameter.key}\" is not valid for \"audit\", please remove it!")
+    }
+  }
+}
+
+def test(Map config = [:]){
+  config.subCommand = 'test'
+  validParameters = [
+    'dockerImage':'',
+    'subCommand':'',
+    'dockerAdditionalMounts':'',
+    'dockerEnvironmentVariables':''
+  ]
+  for ( parameter in config ) {
+    if ( !validParameters.containsKey(parameter.key)){
+      error("yarn - Parameter \"${parameter.key}\" is not valid for \"test\", please remove it!")
+    }
+  }
+}
+
+def install(Map config = [:]){
+  config.subCommand = 'install'
+  validParameters = [
+    'dockerImage':'',
+    'subCommand':'',
+    'dockerAdditionalMounts':'',
+    'dockerEnvironmentVariables':''
+  ]
+  for ( parameter in config ) {
+    if ( !validParameters.containsKey(parameter.key)){
+      error("yarn - Parameter \"${parameter.key}\" is not valid for \"install\", please remove it!")
+    }
+  }
+}
+
+def call(Map config = [:]){
+  yarnCommand = dockerRunCommand(
+    dockerImage: config.dockerImage,
+    fallbackCommand: 'yarn',
+    additionalMounts: config.dockerAdditionalMounts,
+    environmentVariables: config.dockerEnvironmentVariables
+  )
+
+  execute(
+    script: "${yarnCommand} --version"
+  )
+
+  return execute(
+    script: "${yarnCommand} ${config.subCommand}"
+  )
+}
