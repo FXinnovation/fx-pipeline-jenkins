7b2dca303ca12c295572cb88777e2e5f498a3148
Pascal Clermont <pascal.clermont@fxinnovation.com>
2019-04-11 23:54:20 -0400

    testing some parallel work

--- vars/fxPython.groovy
+++ vars/fxPython.groovy
@@ -1,45 +1,124 @@
-def call(Map config = [:]) {
-
+def call(Map closures = [:], List propertiesConfig = []){
     mapAttributeCheck(config, 'version', CharSequence, '3')
 
-    parallel fxJob([
-            pipeline: { Map scmInfo ->
-                def isTagged = '' != scmInfo.tag
-                def MakefileFileExists = fileExists 'Makefile'
-                def toDeploy = false
+    defaultPropertiesConfig = [
+            buildDiscarder(
+                    logRotator(
+                            artifactDaysToKeepStr: '',
+                            artifactNumToKeepStr: '10',
+                            daysToKeepStr: '',
+                            numToKeepStr: '10'
+                    )
+            ),
+            pipelineTriggers(
+                    [cron('@midnight')]
+            )
+    ]
 
-                if (isTagged && MakefileFileExists && jobInfo.isManuallyTriggered()) {
-                    toDeploy = true
+//    properties(defaultPropertiesConfig + propertiesConfig)
+    status='SUCCESS'
+    def label2 = UUID.randomUUID().toString()
+    podTemplate(
+            cloud: 'kubernetes',
+            name:  'jenkins-slave-linux',
+            namespace: 'default',
+            nodeUsageMode: 'NORMAL',
+            idleMinutes: 0,
+            slaveConnectTimeout: 100,
+            podRetention: never(),
+            label: label2,
+            containers: [
+                    containerTemplate(
+                            name: 'jnlp',
+                            image: "fxinnovation/jenkinsk8sslave:latest",
+                            args: '${computer.jnlpmac} ${computer.name}',
+                            privileged: true,
+                            alwaysPullImage: true,
+                            workingDir: '/home/jenkins',
+                            resourceRequestCpu: '100m',
+                            resourceLimitCpu: '1',
+                            resourceRequestMemory: '1024Mi',
+                            resourceLimitMemory: '2048Mi'
+                    )
+            ]
+    )
+    def label = UUID.randomUUID().toString()
+    podTemplate(
+            cloud: 'kubernetes',
+            name:  'jenkins-slave-linux',
+            namespace: 'default',
+            nodeUsageMode: 'NORMAL',
+            idleMinutes: 0,
+            slaveConnectTimeout: 100,
+            podRetention: never(),
+            label: label,
+            containers: [
+                    containerTemplate(
+                            name: 'jnlp',
+                            image: "fxinnovation/jenkinsk8sslave:latest",
+                            args: '${computer.jnlpmac} ${computer.name}',
+                            privileged: true,
+                            alwaysPullImage: true,
+                            workingDir: '/home/jenkins',
+                            resourceRequestCpu: '100m',
+                            resourceLimitCpu: '1',
+                            resourceRequestMemory: '1024Mi',
+                            resourceLimitMemory: '2048Mi'
+                    )
+            ]
+    ){
+        node(label){
+            try{
+                ansiColor('xterm') {
+                    stage('prepare'){
+                        if (closures.containsKey('prePrepare')){
+                            closures.prePrepare()
+                        }
+                        scmInfo = fxCheckout()
+                        if (closures.containsKey('postPrepare')){
+                            closures.postPrepare()
+                        }
+                    }
+                    parallel lint: {
+                        node(label1) {
+                            pipelinePython([
+                                    version: config.version,
+                                    stage: 'lint'
+                            ])
+                        }
+                    },
+                    test: {
+                        pipelinePython([
+                                version: config.version,
+                                stage: 'test'
+                        ])
+                    }
                 }
-
-                printDebug("isTagged: ${isTagged} | MakefileFileExists: ${MakefileFileExists} | manuallyTriggered: ${jobInfo.isManuallyTriggered()} | toDeploy:${toDeploy}")
-
-                pipelinePython([
-                        version: config.version,
-                        stage: 'lint'
-                ])
-
-            }
-    ]),
-   fxJob([
-            pipeline: { Map scmInfo ->
-                def isTagged = '' != scmInfo.tag
-                def MakefileFileExists = fileExists 'Makefile'
-                def toDeploy = false
-
-                if (isTagged && MakefileFileExists && jobInfo.isManuallyTriggered()) {
-                    toDeploy = true
+            }catch(error){
+                status='FAILURE'
+                throw error
+            }finally{
+                stage('notify'){
+                    if (closures.containsKey('preNotify')){
+                        closures.preNotify()
+                    }
+                    fx_notify(
+                            status: status
+                    )
+                    if (closures.containsKey('postNotify')){
+                        closures.postNotify()
+                    }
                 }
-
-                printDebug("isTagged: ${isTagged} | MakefileFileExists: ${MakefileFileExists} | manuallyTriggered: ${jobInfo.isManuallyTriggered()} | toDeploy:${toDeploy}")
-
-                pipelinePython([
-                        version: config.version,
-                        stage: 'test'
-                ])
-
+                stage('cleanup'){
+                    if (closures.containsKey('preCleanup')){
+                        closures.preCleanup()
+                    }
+                    cleanWs()
+                    if (closures.containsKey('postCleanup')){
+                        closures.postCleanup()
+                    }
+                }
             }
-    ])
-
-
+        }
+    }
 }
