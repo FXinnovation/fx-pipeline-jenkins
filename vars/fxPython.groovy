93956b2d048f156f50415ab998a104ed0a56101d
Pascal Clermont <pascal.clermont@fxinnovation.com>
2019-04-11 23:14:40 -0400

    testing some parallel work

--- vars/fxPython.groovy
+++ vars/fxPython.groovy
@@ -1,5 +1,4 @@
 def call(Map config = [:]) {
-
     mapAttributeCheck(config, 'version', CharSequence, '3')
 
     fxJob([
@@ -16,13 +15,28 @@ def call(Map config = [:]) {
 
                 pipelinePython([
                         version: config.version,
-                        stage: 'lint'
-                ])
-                pipelinePython([
-                        version: config.version,
-                        stage: 'test'
+                        stage  : 'lint'
                 ])
 
+            }
+    ])
+
+    fxJob([
+            pipeline: { Map scmInfo ->
+                def isTagged = '' != scmInfo.tag
+                def MakefileFileExists = fileExists 'Makefile'
+                def toDeploy = false
+
+                if (isTagged && MakefileFileExists && jobInfo.isManuallyTriggered()) {
+                    toDeploy = true
+                }
+
+                printDebug("isTagged: ${isTagged} | MakefileFileExists: ${MakefileFileExists} | manuallyTriggered: ${jobInfo.isManuallyTriggered()} | toDeploy:${toDeploy}")
+
+                pipelinePython([
+                        version: config.version,
+                        stage  : 'test'
+                ])
             }
     ])
 }
