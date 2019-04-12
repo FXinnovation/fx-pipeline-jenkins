e926e29696c104efae3f6d0281f3c0ae85066147
Pascal Clermont <pascal.clermont@fxinnovation.com>
2019-04-12 10:05:37 -0400

    adding debug

--- vars/pipelinePython.groovy
+++ vars/pipelinePython.groovy
@@ -39,82 +39,91 @@ def call(Map config = [:], Map closures = [:]) {
 
 //    parallel branches
 
-    stage('Virtual Env') {
-                       virtualenv(config, closures)
-                    }
-    stage('Run Tests') {
+    stage('Parallel Stage') {
+        when {
+            branch 'master'
+        }
+        failFast true
         parallel {
-            stage('Unittests') {
+            stage('Branch A') {
                 steps {
-                    stage('Unittests') {
-                        test(config, closures)
-                    }
+                    echo "On Branch A"
                 }
             }
-            stage('Lint') {
+            stage('Branch B') {
                 steps {
-                    stage('Lint') {
-                        lint(config, closures)
+                    echo "On Branch B"
+                }
+            }
+            stage('Branch C') {
+                stages {
+                    stage('Nested 1') {
+                        steps {
+                            echo "In stage Nested 1 within Branch C"
+                        }
+                    }
+                    stage('Nested 2') {
+                        steps {
+                            echo "In stage Nested 2 within Branch C"
+                        }
                     }
                 }
             }
         }
-    }
-
 //  stage('coverage') {
 //    virtualenv(config, closures)
 //    coverage(config, closures)
 //  }
 
 //  publish(config, closures)
-}
-
-def virtualenv(Map config = [:], Map closures = [:]) {
-    mapAttributeCheck(config, 'version', CharSequence, '3')
-    if (!closures.containsKey('virtualenv')) {
-        closures.virtualenv = {
-            python.virtualenv([
-                    version: config.version
-            ])
-        }
     }
-    closures.virtualenv()
-}
 
-
-def test(Map config = [:], Map closures = [:]) {
-    mapAttributeCheck(config, 'version', CharSequence, '3')
-    if (!closures.containsKey('test')) {
-        closures.test = {
-            python.test([
-                    version: config.version
-            ])
+    def virtualenv(Map config = [:], Map closures = [:]) {
+        mapAttributeCheck(config, 'version', CharSequence, '3')
+        if (!closures.containsKey('virtualenv')) {
+            closures.virtualenv = {
+                python.virtualenv([
+                        version: config.version
+                ])
+            }
         }
+        closures.virtualenv()
     }
-    try {
-        replay = closures.test()
-        if (replay.stdout =~ /FAILED \(errors=.*\)/) {
-            error('Some tests have not passed.')
-        }
-    } catch (error) {
-        throw (error)
-    } finally {
-        junit '**/Reports/*.xml'
-    }
-}
 
-def lint(Map config = [:], Map closures = [:]) {
-    mapAttributeCheck(config, 'version', CharSequence, '3')
-    if (!closures.containsKey('lint')) {
-        closures.lint = {
-            python.lint([
-                    version: config.version,
-            ])
+
+    def test(Map config = [:], Map closures = [:]) {
+        mapAttributeCheck(config, 'version', CharSequence, '3')
+        if (!closures.containsKey('test')) {
+            closures.test = {
+                python.test([
+                        version: config.version
+                ])
+            }
+        }
+        try {
+            replay = closures.test()
+            if (replay.stdout =~ /FAILED \(errors=.*\)/) {
+                error('Some tests have not passed.')
+            }
+        } catch (error) {
+            throw (error)
+        } finally {
+            junit '**/Reports/*.xml'
         }
     }
 
-    closures.lint()
+    def lint(Map config = [:], Map closures = [:]) {
+        mapAttributeCheck(config, 'version', CharSequence, '3')
+        if (!closures.containsKey('lint')) {
+            closures.lint = {
+                python.lint([
+                        version: config.version,
+                ])
+            }
+        }
 
-}
+        closures.lint()
+
+    }
 
 
