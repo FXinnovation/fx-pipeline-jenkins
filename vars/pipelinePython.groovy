e4eff77e6f850084ef28b58240bc99c886723bcd
Pascal Clermont <pascal.clermont@fxinnovation.com>
2019-04-15 07:19:02 -0400

    putting everything back in sequential order

--- vars/pipelinePython.groovy
+++ vars/pipelinePython.groovy
@@ -7,62 +7,17 @@ def call(Map config = [:], Map closures = [:]) {
     mapAttributeCheck(config, 'version', CharSequence, '3')
     // in this array we'll place the jobs that we wish to run
 
-    def branches = [:]
-//
-//    branches["Unit Tests"] = {
-//        fxSingleJob([
-//                pipeline: { Map scmInfo ->
-//                    stage('Virtual Env') {
-//                        virtualenv(config, closures)
-//                    }
-//
-//                    stage('Unit Tests') {
-//                        test(config, closures)
-//                    }
-//                }
-//        ])
-//    }
-//
-//    branches["Lint"] = {
-//        fxSingleJob([
-//                pipeline: { Map scmInfo ->
-//                    stage('Virtual Env') {
-//                        virtualenv(config, closures)
-//                    }
-//
-//                    stage('Lint') {
-//                        lint(config, closures)
-//                    }
-//                }
-//        ])
-//    }
-
-//    parallel branches
-
     stage('Virtual Env') {
         virtualenv(config, closures)
     }
 
-    branches["Lint"] = {
-        stage('Lint') {
-            lint(config, closures)
-        }
-    }
-    branches["Unittest"] = {
-        stage('Unittest') {
-            test(config, closures)
-        }
-    }
-    branches["C"] = {
-        stage('Branch C') {
-            echo "On Branch C"
-        }
-        stage('Branch D') {
-            echo "On Branch D"
-        }
-    }
 
-    parallel branches
+    stage('Lint') {
+        lint(config, closures)
+    }
+    stage('Unittest') {
+        test(config, closures)
+    }
 
 //  stage('coverage') {
 //    virtualenv(config, closures)
