0cc35818e0bf8e29f69175f81fa21ca9feeaedc7
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-04-24 14:48:27 -0400

    refactor: changes dockerRunCommand to use mapAttributes

--- vars/dockerRunCommand.groovy
+++ vars/dockerRunCommand.groovy
@@ -1,27 +1,18 @@
 def call(Map config = [:]) {
-  if ( !config.containsKey('dockerImage') || !(config.dockerImage instanceof CharSequence) ){
-    error('dockerRunCommand - "dockerImage" parameter must exists and be a String (implements CharSequence).')
-  }
+  mapAttributeCheck(config, 'dockerImage', CharSequence, '', 'dockerRunCommand - "dockerImage" parameter must exists and be a String (implements CharSequence).')
+  mapAttributeCheck(config, 'fallbackCommand', CharSequence, '', 'dockerRunCommand - "fallbackCommand" parameter must exists and be a String (implements CharSequence).')
 
-  if ( !config.containsKey('fallbackCommand') || !(config.fallbackCommand instanceof CharSequence) ){
-    error('dockerRunCommand - "fallbackCommand" parameter must exists and be a String (implements CharSequence).')
-  }
-
-  if ( !config.containsKey('additionalMounts') || !(config.additionalMounts instanceof Map) ){
-    config.additionalMounts = []
-  }
-
-  if ( !config.containsKey('environmentVariables')  || !(config.environmentVariables instanceof Map) ){
-    config.environmentVariables = []
-  }
+  mapAttributeCheck(config, 'additionalMounts', Map, [])
+  mapAttributeCheck(config, 'command', CharSequence, '')
+  mapAttributeCheck(config, 'environmentVariables', Map, [])
 
   try {
     execute(
       script: 'docker version'
     )
   } catch(dockerVersionError) {
-    if ( "" == config.fallbackCommand ){
-      println "Docker is not available, assuming the tool is installed."
+    if ( '' == config.fallbackCommand ){
+      println 'Docker is not available, assuming the tool is installed.'
     }else{
       println "Docker is not available, assuming ${config.fallbackCommand} is installed."
     }
@@ -32,15 +23,15 @@ def call(Map config = [:]) {
     script: "docker pull ${config.dockerImage}"
   )
 
-  def additionalMounts = ""
+  def additionalMounts = ''
 
   config.additionalMounts.each{
     key, value -> additionalMounts += "-v ${key}:\"${value}\" "
   }
-  def environmentVariables = ""
+  def environmentVariables = ''
   config.environmentVariables.each{
     key, value -> environmentVariables += "-e ${key}=\"${value}\" "
   }
 
-  return "docker run --rm -v \$(pwd):/data ${additionalMounts} ${environmentVariables} -w /data ${config.dockerImage}"
+  return "docker run --rm -v \$(pwd):/data ${additionalMounts} ${environmentVariables} -w /data ${config.dockerImage} ${config.command}"
 }
