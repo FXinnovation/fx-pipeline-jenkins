016d72aff9dc42369fbde5776b4bd52aa197a6cb
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-05-02 10:23:31 -0400

    Made namespace optionnal because ECR

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -2,17 +2,24 @@ def build(Map config = [:]){
   mapAttributeCheck(config, 'image', CharSequence, '', 'The image key must be defined')
   mapAttributeCheck(config, 'tags', List, [], 'This tags key must be defined')
   mapAttributeCheck(config, 'registries', List, [])
-  mapAttributeCheck(config, 'namespace', CharSequence, '', 'The namespace key must be defined')
+  mapAttributeCheck(config, 'namespace', CharSequence, '')
 
   optionsString = ''
   config.tags.each { tag ->
     optionsString += '--tag '
     if (config.containsKey('registries') && [] != config.registries){
       config.registries.each { registry ->
-        optionsString += "${registry}/${config.namespace}/${config.image}:${tag} "
+        optionsString += "${registry}/"
+        if (config.namespace != '') {
+          optionsString += "${config.namespace}/"
+        }
+        optionsString += "${config.image}:${tag} "
       }
     }
-    optionsString += "${config.namespace}/${config.image}:${tag} "
+    if (config.namespace != '') {
+      optionsString += "${config.namespace}/"
+    }
+    optionsString += "${config.image}:${tag} "
   }
 
   execute(
@@ -43,13 +50,20 @@ def publish(Map config = [:]){
     optionsString = ''
     if (config.containsKey('registries') && [] != config.registries){
       config.registries.each { registry ->
-        optionsString += "${registry}/${config.namespace}/${config.image}:${tag} "
+        optionsString += "${registry}/"
+        if (config.namespace != '') {
+          optionsString += "${config.namespace}/"
+        }
+        optionsString += "${config.image}:${tag} "
         execute(
           script: "docker push ${optionsString}"
         )
       }
     }else{
-      optionsString += "${config.namespace}/${config.image}:${tag} "
+      if (config.namespace != '') {
+        optionsString += "${config.namespace}/"
+      }
+      optionsString += "${config.image}:${tag} "
     }
     execute(
       script: "docker push ${optionsString}"
