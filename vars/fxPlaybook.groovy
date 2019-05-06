f70ede3b925e6c7027969f7a9dfd964e433a8868
Julien Cabillot <julien.cabillot@fxinnovation.com>
2019-05-06 14:08:46 -0400

    sync

--- vars/fxPlaybook.groovy
+++ vars/fxPlaybook.groovy
@@ -1,47 +1,99 @@
 def call(Map config = [:], Map closures = [:]) {
-  mapAttributeCheck(config, 'ansiblelintConfig',     Map, [:])
-  mapAttributeCheck(config, 'ansiblelintOutputFile', CharSequence, 'ansible-lint.txt')
-
-  if (!closures.containsKey('converge')){
-    mapAttributeCheck(config, 'awsSSHKeyId',           CharSequence, '', '“awsSSHKeyId” parameter is mandatory without custom closure “converge”.')
-    mapAttributeCheck(config, 'awsAccessKeyId',        CharSequence, '', '“awsAccessKeyId” parameter is mandatory without custom closure “converge”.')
-    mapAttributeCheck(config, 'awsSecretAccessKey',    CharSequence, '', '“awsSecretAccessKey” parameter is mandatory without custom closure “converge”.')
-    mapAttributeCheck(config, 'awsRegion',             CharSequence, '', '“awsRegion” parameter is mandatory without custom closure “converge”.')
-    mapAttributeCheck(config, 'awsSubnetId',           CharSequence, '', '“awsSubnetId” parameter is mandatory without custom closure “converge”.')
-    mapAttributeCheck(config, 'awsIAMProfile',         CharSequence, '', '“awsIAMProfile” parameter is mandatory without custom closure “converge”.')
-    mapAttributeCheck(config, 'kitchenIdempotency',    CharSequence, '', '“kitchenIdempotency” parameter is mandatory without custom closure “converge”.')
-    mapAttributeCheck(config, 'kitchenAnsibleVersion', CharSequence, 'latest')
-    mapAttributeCheck(config, 'kitchenPrivateKeyPath', CharSequence, '', '“kitchenPrivateKeyPath” parameter is mandatory without custom closure “converge”.')
-
-    closures.converge = {
-      kitchen.test(
-        dockerEnvironmentVariables: [
-          AWS_SSH_KEY_ID:          "${config.awsSSHKeyId}",
-          AWS_SSH_KEY:             '/id_rsa.pem',
-          AWS_ACCESS_KEY_ID:       "${config.awsAccessKeyId}",
-          AWS_SECRET_ACCESS_KEY:   "${config.awsSecretAccessKey}",
-          AWS_REGION:              "${config.awsRegion}",
-          AWS_SUBNETID:            "${config.awsSubnetId}",
-          AWS_IAMPROFILE:          "${config.awsIAMProfile}",
-          KITCHEN_IDEMPOTENCY:     "${config.kitchenIdempotency}",
-          KITCHEN_ANSIBLE_VERSION: "${config.kitchenAnsibleVersion}",
-          KITCHEN_ROLENAME:        "${config.kitchenRoleName}"
-        ],
-        dockerAdditionalMounts: [
-          (config.kitchenPrivateKeyPath): '/id_rsa.pem'
-        ]
-      )
-    }
-  }
+  mapAttributeCheck(config, 'ansiblelintConfig',      Map,          [:])
+  mapAttributeCheck(config, 'ansiblelintOutputFile',  CharSequence, 'ansible-lint.txt')
+  mapAttributeCheck(config, 'awsIAMProfile',          CharSequence, 'testjc_todelete')
+  mapAttributeCheck(config, 'awsRegion',              CharSequence, 'us-east-1')
+  mapAttributeCheck(config, 'awsSubnetId',            CharSequence, 'subnet-cd28dca9')
+  mapAttributeCheck(config, 'kitchenAwsCredentialId', CharSequence, 'itoa-application-awscollectors-awscred')
+  mapAttributeCheck(config, 'kitchenAnsibleVersion',  CharSequence, '2.7.10')
+  mapAttributeCheck(config, 'kitchenIdempotency',     CharSequence, 'true')
+  mapAttributeCheck(config, 'kitchenSshCredentialId', CharSequence, 'fxlab_jenkins')
+  mapAttributeCheck(config, 'junitReport',            CharSequence, '*_inspec.xml')
+  mapAttributeCheck(config, 'scmSshCredentialId',     CharSequence, 'gitea-fx_administrator-key')
+  mapAttributeCheck(config, 'publish',                Boolean,      false)
+  mapAttributeCheck(config, 'initSSHHostKeys',        List,         [
+    '|1|V8Cs9iHiY6IHtysrmCUykgmnQEI=|CNbB5msCLZaO4ne4HAdyHNr93eo= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDD4T37qQ3qJENiwfVwog8UfWlMg3Rl6PhEeF3vD1kvSpGjyF88XqgGsdDzTvFYeoc+WhbPQQXqIiowoovh6W4LkNi7SOTf10po0Llxde/xSZc32zQ4fltERf69L2XvQy43a5Apx1GgcLQrbaRj1/zx4Muo3hjvDu/OPkhso6Q734lfgZcy1uFoXaZIadeJOVzQIez3FiAmmr6r48Eb7hntK57u+Xdpd5Fq9zBDoMbzAnsCXZWzYEC/j9Hje+wV5iwyM/UWUaC06zyfG8NvPkwU90mIVxIX6NB9yrGlT0tPuNL69Vz4Ykc9LoHJQoHcetqbzS684vvwDnlXP0TrC/AV',
+    '|1|+TYoo5BCu7BRQZl0+TXEK2f6JW8=|zfDEKwHmsNmNGPY1bNj1CNjOPLQ= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAZ3HVe71Jte8O3B6CnnCojmCtQJidELmlSiKbxZphEwnhl6Wr7iF0GH+Oo5k34Q8toPHvmIRPh9UcNTr4g5dHI=',
+    '|1|6Kd9WNT+MUC06McJma+j91Fxfik=|aHMHcoEUNdmmgU75HWXfkIGTUWA= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnKb68pJWPerdWlT9m2DraaFalb7K562kUO4SHtpyaL',
+  ])
 
   fxJob([
     pipeline: { Map scmInfo ->
-      config.kitchenRoleName = "${scmInfo.repositoryName}"
-      pipelinePlaybook(config, closures)
+      withCredentials([
+        sshUserPrivateKey(
+          credentialsId:   config.scmSshCredentialId,
+          keyFileVariable: 'scmKey'
+        ),
+        sshUserPrivateKey(
+          credentialsId:   config.kitchenSshCredentialId,
+          keyFileVariable: 'kitchenKey'
+        ),
+        usernamePassword(
+          credentialsId:    config.kitchenAwsCredentialId,
+          usernameVariable: 'access_key',
+          passwordVariable: 'secret_key'
+        )
+      ]) {
+        sshagent([config.scmSshCredentialId]) {
+          if (!closures.containsKey('converge')){
+            closures.converge = {
+              try {
+                kitchen.test(
+                  dockerEnvironmentVariables: [
+                    AWS_SSH_KEY_ID:          "${config.kitchenSshCredentialId}",
+                    AWS_SSH_KEY:             '/id_rsa.pem',
+                    AWS_ACCESS_KEY_ID:       "${access_key}",
+                    AWS_SECRET_ACCESS_KEY:   "${secret_key}",
+                    AWS_REGION:              "${config.awsRegion}",
+                    AWS_SUBNETID:            "${config.awsSubnetId}",
+                    AWS_IAMPROFILE:          "${config.awsIAMProfile}",
+                    KITCHEN_IDEMPOTENCY:     "${config.kitchenIdempotency}",
+                    KITCHEN_ANSIBLE_VERSION: "${config.kitchenAnsibleVersion}",
+                    KITCHEN_ROLENAME:        "${scmInfo.repositoryName}"
+                  ],
+                  dockerAdditionalMounts: [
+                    (kitchenKey): '/id_rsa.pem'
+                  ]
+                )
+              } catch(kitchenError) {
+                archiveArtifacts(
+                  allowEmptyArchive: false,
+                  artifacts:         '.kitchen/**/*'
+                )
+                throw(kitchenError)
+              } finally {
+                archiveArtifacts(
+                  allowEmptyArchive: true,
+                  artifacts:         config.junitReport
+                )
+                junit config.junitReport
+              }
+            }
+          }
+
+          pipelinePlaybook(
+            [
+              ansiblelintConfig:     config.ansiblelintConfig,
+              galaxySSHHostKeys:     config.initSSHHostKeys,
+              galaxyAgentSocket:     '\$(readlink -f $SSH_AUTH_SOCK)',
+              awsSSHKeyId:           "${config.kitchenSshCredentialId}",
+              awsAccessKeyId:        "${access_key}",
+              awsSecretAccessKey:    "${secret_key}",
+              awsRegion:             "${config.awsRegion}",
+              awsSubnetId:           "${config.awsSubnetId}",
+              awsIAMProfile:         "${config.awsIAMProfile}",
+              kitchenIdempotency:    "${config.kitchenIdempotency}",
+              kitchenAnsibleVersion: "${config.kitchenAnsibleVersion}",
+              kitchenPrivateKeyPath: "${kitchenKey}",
+              publish:               config.publish
+            ],
+            closures
+          )
+        }
+      }
     },
     preNotify: {
       def issues = scanForIssues(
-        blameDisabled: true,
         tool: pyLint(
           name: 'ansible-lint',
           pattern: config.ansiblelintOutputFile
