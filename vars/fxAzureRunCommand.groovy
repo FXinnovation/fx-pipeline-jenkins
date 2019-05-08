8fd4c0844fb08e4ff4a454b49edb4652ee012d01
Quentin Vallin <quentin.vallin@fxinnovation.com>
2019-05-08 09:55:50 -0400

    Add fxAzureRunCommand helper

---
+++ vars/fxAzureRunCommand.groovy
@@ -0,0 +1,36 @@
+def call(Map config = [:]) {
+
+  mapAttributeCheck(config, 'credentialAzure', CharSequence, '', '“credentialAzure” parameter is mandatory.')
+  mapAttributeCheck(config, 'resourceGroupName', CharSequence, '', '“resourceGroupName” parameter is mandatory.')
+  mapAttributeCheck(config, 'vitualMachineName', CharSequence, '', '“virtualMachineName” parameter is mandatory.')
+  mapAttributeCheck(config, 'scriptPath', CharSequence, '', '“scriptPath” parameter is mandatory.')
+  mapAttributeCheck(config, 'tenantId', CharSequence, '', '“tenantId” parameter is mandatory.')
+  mapAttributeCheck(config, 'libFolder', CharSequence, 'fxinnovation-common-scripts-powershell')
+  mapAttributeCheck(config, 'libVersion', CharSequence, '0.0.21')
+  mapAttributeCheck(config, 'powershellDockerImage', CharSequence, 'fxinnovation/powershell:latest')
+  mapAttributeCheck(config, 'scriptParameters', CharSequence, '')
+
+  if('' != config.scriptParameters) {
+    config.scriptParameters = "-Parameters \"${config.runbookOptions}\""
+  }
+
+  // This is temporary since we need to create nugget repo
+  fxCheckoutTag (
+    directory: config.libFolder,
+    credentialsId: 'jenkins_fxinnovation_bitbucket',
+    repoUrl: 'https://bitbucket.org/fxadmin/fxinnovation-common-scripts-powershell.git',
+    tag: config.libVersion
+  )
+  withCredentials([
+    usernamePassword(
+      credentialsId: config.credentialAzure,
+      usernameVariable: 'appId',
+      passwordVariable: 'servicePrincipalPassword'
+    )
+  ]) {
+    executePowershell([
+      dockerImage: config.powershellDockerImage,
+      script: "/data/${config.libFolder}/CodeSnipetCICD/Run-AzureCommand.ps1 -ResourceGroupName \"${config.resourceGroupName}\" -VirtualMachineName \"${config.vitualMachineName}\" -ScriptPath \"${config.scriptPath}\" -Tenant \"${config.tenantId}\" -AppId \"${appId}\" -Password \"${servicePrincipalPassword}\" ${config.scriptParameters} -Verbose "
+    ])
+  }
+}
