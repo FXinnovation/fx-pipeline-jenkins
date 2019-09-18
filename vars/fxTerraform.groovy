46565f766b8bda3aeffd70a5f6558cd2e3264215
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-09-17 23:29:49 -0400

    Revert "Added feature"
    This reverts commit 7e556bcad406d3e741ee6b75639e9f5ad19a4517.

--- vars/fxTerraform.groovy
+++ vars/fxTerraform.groovy
@@ -12,14 +12,6 @@ def call(Map config = [:], Map closures = [:]) {
   mapAttributeCheck(config, 'terraformInitBackendConfigsTest', ArrayList, [])
   mapAttributeCheck(config, 'terraformInitBackendConfigsPublish', ArrayList, [])
   mapAttributeCheck(config, 'commonOptions', Map, [:])
-  if (!closures.containsKey('notify')){
-    closures.notify = {
-      fx_notify(
-        status: status,
-        failOnError: false
-      )
-    }
-  }
 
   // commandTargets is deprecated - to be removed once Jenkinsfile are update not to contain commandTargets.
   if (config.containsKey('commandTargets')) {
@@ -28,7 +20,6 @@ def call(Map config = [:], Map closures = [:]) {
 
   fxJob([
     postPrepare: closures.postPrepare,
-    notify: closures.notify,
     pipeline: { Map scmInfo ->
       def isTagged = '' != scmInfo.tag
       def deployFileExists = fileExists 'deploy.tf'
