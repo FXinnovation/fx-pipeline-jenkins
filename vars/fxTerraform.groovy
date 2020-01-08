643d8f06763fa6602d6c4312c819d3ddfef90f74
Quentin Vallin <quentin.vallin@fxinnovation.com>
2020-01-08 10:34:07 -0500

    fix/ typo 2

--- vars/fxTerraform.groovy
+++ vars/fxTerraform.groovy
@@ -19,7 +19,7 @@ def call(Map config = [:], Map closures = [:]) {
     print('DEPRECATED WARNING: please remove “commandTargets” attribute from your Jenkinsfile as it’s not used anymore. Once all Jenkinsfiles are updated, remove this message.')
   }
 
-  closures.pipeline: { ScmInfo scmInfo ->
+  closures.pipeline = { ScmInfo scmInfo ->
     def deployFileExists = fileExists 'deploy.tf'
     def toDeploy = false
 
