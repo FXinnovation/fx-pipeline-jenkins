89151d098831325c75e91a21c73dd6c25e788d42
Quentin Vallin <quentin.vallin@fxinnovation.com>
2020-01-08 15:55:19 -0500

    fix/ dbug

--- vars/fxTerraform.groovy
+++ vars/fxTerraform.groovy
@@ -20,6 +20,7 @@ def call(Map config = [:], Map closures = [:]) {
   }
 
   closures.pipeline = { ScmInfo scmInfo ->
+    println scmInfo
     def deployFileExists = fileExists 'deploy.tf'
     def toDeploy = false
 
