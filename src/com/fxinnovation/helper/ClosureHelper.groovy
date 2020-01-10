2c7f1c2f844ced4ed852144c772739862a14f66e
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-01-10 09:13:31 -0500

    fix: passes context to CloserHelper to allows stage()

--- src/com/fxinnovation/helper/ClosureHelper.groovy
+++ src/com/fxinnovation/helper/ClosureHelper.groovy
@@ -1,31 +1,29 @@
 package com.fxinnovation.helper
 
+import groovy.lang.Script
+
 class ClosureHelper {
   private Map closures
+  private Script context
 
   ClosureHelper(Map closures) {
     this.closures = closures
+    this.context = context
   }
 
   Boolean isDefined(String closureName) {
     return this.closures.containsKey(closureName) && Closure.isInstance(this.closures[closureName])
   }
 
-  /**
-   * Executes a closure. NOT USABLE WITH JENKINS SECURITY SETTINGS BY DEFAULT.
-   */
   public execute(String closureName, ...args) {
     // As today (2020-01) CPS does not support spread: “spread not yet supported for CPS transformation”.
     // Therefore, passing variables to closure is not yet supported. Arguments passed to closure below should be “*args” (works in vanilla groovy).
     return this.closures[closureName]()
   }
 
-  /**
-   * Executes a closure. NOT USABLE WITH JENKINS SECURITY SETTINGS BY DEFAULT.
-   */
   public executeWithinStage(String closureName, ...args) {
     if (this.isDefined(closureName)) {
-      stage(closureName) {
+      this.context.stage(closureName) {
         // @See the comment at the execute method of this object. Arguments passed to closure below should be “*args” (works in vanilla groovy).
         return this.execute(closureName, args)
       }
