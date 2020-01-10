b259f3818407df64a683e4d6a7b12104f8181f51
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-01-10 07:26:40 -0500

    feat: changes pipeline docker behavio

--- vars/pipelineDocker.groovy
+++ vars/pipelineDocker.groovy
@@ -1,40 +1,81 @@
-def call(Map config = [:], Map closures =[:]){
-  if (!config.containsKey('publish')){
-    config.publish = false
-  }
-  if (!config.containsKey('dockerBuild')){
-    error('dockerBuild configuration is mandatory')
-  }
-  if (!config.containsKey('dockerPublish') && true == config.publish){
-    error('dockerPublish configuration is mandatory when config.publish is set to true')
+import com.fxinnovation.data.ScmInfo
+import com.fxinnovation.helper.ClosureHelper
+
+def call(Map config = [:], Map closures = [:], ScmInfo scmInfo){
+  mapAttributeCheck(config, 'disablePublish', Boolean, false)
+  mapAttributeCheck(config, 'dockerBuild', Map)
+  mapAttributeCheck(config, 'dockerPublish', Map)
+
+  closureHelper = new ClosureHelper(closures)
+
+  closureHelper.executeWithinStage('preBuild')
+
+  stage('build') {
+    for (registry in config.dockerPublish.registries) {
+      dockerImage.build(config.dockerBuild + [tags: this.getAllTags()] + [registry: registry])
+    }
   }
 
-  if (closures.containsKey('preBuild') && closures.preBuild instanceof Closure){
-    stage('preBuild'){
-      closures.preBuild()
-    }
+  closureHelper.executeWithinStage('postBuild')
+
+  if (!config.disablePublish) {
+    println 'Skip publication because “disablePublish” = true.'
   }
-  stage('build'){
-    dockerImage.build(config.dockerBuild)
+
+  if (!scmInfo.isPublishableAsAnything()) {
+    println 'Skip publication because this commit is not publishable as anything.'
   }
-  if (closures.containsKey('postBuild') && closures.postBuild instanceof Closure){
-    stage('postBuild'){
-      closures.postBuild()
-    }
+
+  closureHelper.executeWithinStage('prePublish')
+
+  this.publish(config, scmInfo)
+  this.publishDev(config, scmInfo)
+
+  closureHelper.executeWithinStage('postPublish')
+}
+
+private void publish(Map config, ScmInfo scmInfo) {
+  if (!scmInfo.isPublishable()) {
+    println 'Skip *latest tag* publication because this commit is not publishable as latest version.'
+    return
   }
-  if (config.publish){
-    if (closures.containsKey('prePublish') && closures.prePublish instanceof Closure){
-      stage('prePublish'){
-        closures.prePublish()
+
+  stage('publish') {
+    for (registry in config.dockerPublish.registries) {
+      if (this.dockerTagExists(config.dockerPublish.namespace, registry, scmInfo.getPatchTag())) {
+        println "Skip publication for “${scmInfo.getPatchTag()}” in “${registry}” because this version was already published."
+        return
       }
-    }
-    stage('publish'){
-      dockerImage.publish(config.dockerPublish)
-    }
-    if (closures.containsKey('postPublish') && closures.postPublish instanceof Closure){
-      stage('postPublish'){
-        closures.postBuild()
-      }
+      dockerImage.publish(config.dockerPublish + [tags: this.getAllTags()] + [registry: registry])
     }
   }
+}
+
+private void publishDev(Map config, ScmInfo scmInfo) {
+  if (!scmInfo.isPublishableAsDev()) {
+    println 'Skip *dev* publication because this commit is not publishable as a development version.'
+    return
+  }
+
+  stage('publish dev') {
+    config.dockerPublish.registries.each { registry ->
+      dockerImage.publish(config.dockerPublish + [tags: [scmInfo.getTag()]] + [registry: registry])
+    }
+  }
+}
+
+private boolean dockerTagExists(CharSequence registry, CharSequence namespace, CharSequence tag) {
+  return execute(
+    script: "curl --silent -f -lSL ${[registry, namespace, 'tags', tag].removeAll(['']).join('/')} > /dev/null"
+  )
+}
+
+private getAllTags(ScmInfo scmInfo) {
+  return [
+    scmInfo.getBranchAsDockerTag(),
+    scmInfo.getMajorTag(),
+    scmInfo.getMinorTag(),
+    scmInfo.getTag(),
+    'latest',
+  ]
 }
