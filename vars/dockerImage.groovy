108b92f2c38d3094ccff3498ec227cef0004beaf
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-01-14 19:47:40 -0500

    feat: restore old way of login

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -17,9 +17,7 @@ def publish(Map config = [:]) {
         usernameVariable: 'username'
       )
     ]) {
-      execute(script: "echo \'${password}\' > password_file", printCommand: false)
-      execute(script: "cat password_file | docker login --username \'${username}\' --password-stdin ${config.registry}")
-      execute(script: "rm password_file", printCommand: false)
+      execute(script: "docker login --username \'${username}\' --password \'${password}\' ${config.registry}")
     }
   }
 
