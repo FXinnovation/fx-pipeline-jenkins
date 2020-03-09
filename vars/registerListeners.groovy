a9b090929e1934a1cbe37330934fb7e39bdee61d
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-03-09 16:25:33 -0400

    fix: makes sure Checkout is register

--- vars/registerListeners.groovy
+++ vars/registerListeners.groovy
@@ -1,11 +1,18 @@
 import com.fxinnovation.di.IOC
+import com.fxinnovation.event.PipelineEvents
 import com.fxinnovation.listener.standard.*
 import com.fxinnovation.observer.EventDispatcher
+import com.fxinnovation.listener.standard.CheckoutListener
 
 def call() {
   registerServices()
 
   def EventDispatcher eventDispatcher = IOC.get(EventDispatcher.class.getName())
+
+  // ###
+  // General
+  // ###
+  eventDispatcher.attach(IOC.get(CheckoutListener.class.getName()))
 
   // ###
   // Terraform
@@ -18,4 +25,8 @@ def call() {
   eventDispatcher.attach(IOC.get(TerraformPlanListener.class.getName()))
   eventDispatcher.attach(IOC.get(TerraformPlanReplayListener.class.getName()))
   eventDispatcher.attach(IOC.get(TerraformValidateListener.class.getName()))
+
+  eventDispatcher.dispatch(PipelineEvents.PRE_INIT)
+  eventDispatcher.dispatch(PipelineEvents.INIT)
+  eventDispatcher.dispatch(PipelineEvents.POST_INIT)
 }
--- vars/registerServices.groovy
+++ vars/registerServices.groovy
@@ -1,6 +1,4 @@
 import com.fxinnovation.di.IOC
-import com.fxinnovation.event.PipelineEvents
-import com.fxinnovation.event.TerraformEvents
 import com.fxinnovation.observer.EventDispatcher
 import com.fxinnovation.service.standard.ServiceRegisterer
 
@@ -11,9 +9,4 @@ def call() {
 
   ServiceRegisterer serviceRegisterer = new ServiceRegisterer()
   serviceRegisterer.registerAllClasses(this)
-
-  def EventDispatcher eventDispatcher = IOC.get(EventDispatcher.class.getName())
-  eventDispatcher.dispatch(PipelineEvents.PRE_INIT)
-  eventDispatcher.dispatch(PipelineEvents.INIT)
-  eventDispatcher.dispatch(PipelineEvents.POST_INIT)
 }
