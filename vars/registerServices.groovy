be4dcc3056c4cc36e83b621d4c4c5db802584619
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-03-09 16:10:27 -0400

    feat: dispatches INIT events at the beginning of any pipeline

--- vars/registerServices.groovy
+++ vars/registerServices.groovy
@@ -1,6 +1,19 @@
+import com.fxinnovation.di.IOC
+import com.fxinnovation.event.PipelineEvents
+import com.fxinnovation.event.TerraformEvents
+import com.fxinnovation.observer.EventDispatcher
 import com.fxinnovation.service.standard.ServiceRegisterer
 
 def call() {
+  if (ServiceRegisterer.alreadyRegistered) {
+    return
+  }
+
+  def EventDispatcher eventDispatcher = IOC.get(EventDispatcher.class.getName())
+  eventDispatcher.dispatch(PipelineEvents.PRE_INIT)
+  eventDispatcher.dispatch(PipelineEvents.INIT)
+  eventDispatcher.dispatch(PipelineEvents.POST_INIT)
+
   ServiceRegisterer serviceRegisterer = new ServiceRegisterer()
   serviceRegisterer.registerAllClasses(this)
 }
