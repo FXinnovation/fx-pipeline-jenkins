0e17773ba4bc8299b41bb21af1fc9436c645cd29
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2021-02-24 15:45:18 -0500

    test: debug

--- vars/mapAttributeCheck.groovy
+++ vars/mapAttributeCheck.groovy
@@ -1,3 +1,5 @@
+import org.codehaus.groovy.runtime.NullObject
+
 def call(Map mapToCheck, String keyToCheck, Class<?> expectedValueType, defaultValue, String keyUndefinedErrorMessage = '') {
   if ('' == keyToCheck) {
     error('Cannot check a Map’s value with an empty key. Make sure the “keyToCheck” argument has a proper value.')
@@ -7,16 +9,9 @@ def call(Map mapToCheck, String keyToCheck, Class<?> expectedValueType, defaultV
     error(keyUndefinedErrorMessage)
   }
 
-  if (!mapToCheck.containsKey(keyToCheck) || NullObject.isInstance(mapToCheck[keyToCheck])) {
+  if (!(mapToCheck.containsKey(keyToCheck)) || null == mapToCheck[keyToCheck]) {
     mapToCheck[keyToCheck] = defaultValue
   }
-
-  print(keyToCheck)
-  print(mapToCheck)
-  print(mapToCheck.containsKey(keyToCheck))
-  print(defaultValue)
-  print(mapToCheck[keyToCheck])
-  print(mapToCheck[keyToCheck].getClass())
 
   if (!(expectedValueType.isInstance(mapToCheck[keyToCheck]))) {
     error("The type of the value for key “${keyToCheck}” is expected to be “${expectedValueType}”, given: “${mapToCheck[keyToCheck].getClass()}”")
