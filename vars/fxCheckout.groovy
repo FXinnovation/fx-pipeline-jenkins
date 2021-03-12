6079ea0653403e629e5127433578e8d056d08108
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2021-03-12 16:10:59 -0500

    refactor: deprecates fxCheckout

--- vars/fxCheckout.groovy
+++ vars/fxCheckout.groovy
@@ -2,9 +2,12 @@ import com.fxinnovation.deprecation.DeprecatedFunction
 import com.fxinnovation.di.IOC
 import com.fxinnovation.data.ScmInfo
 
+@Deprecated
 def call(Map config = [:]) {
   registerServices()
+
   DeprecatedFunction deprecatedFunction = IOC.get(DeprecatedFunction.class.getName())
+
   return deprecatedFunction.execute({
     return IOC.get(ScmInfo.class.getName())
   }, 'fxCheckout', 'IOC component to get scmInfo: “ScmInfo scmInfo = IOC.get(ScmInfo.class.getName())”.', '01-03-2022')
