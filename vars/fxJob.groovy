7b74afdf25b7341884fb0ae65254079cef21520f
david <david.glantenay@fxinnovation.com>
2021-10-26 16:15:59 -0400

    debug

--- vars/fxJob.groovy
+++ vars/fxJob.groovy
@@ -3,14 +3,16 @@ import com.fxinnovation.di.IOC
 
 def call(Map closures = [:], List propertiesConfig = [], Map config = [:]) {
 
+  def legacyFunction = {
+    standardJob(
+      closureHelper.getClosures(),
+      [
+        disableConcurrentBuilds()
+      ],
+     config
+   )
+  }
+
   def deprecatedFunction = IOC.get(DeprecatedFunction.class.getName())
   deprecatedFunction.execute(legacyFunction, 'fxJob', 'standardJob', '07-01-2022')
-
-  standardJob(
-    closureHelper.getClosures(),
-    [
-      disableConcurrentBuilds()
-    ],
-    config
-  )
 }
