fe817b6d36f4b0270a18add9283850ac25c64211
Charles CEBE <charles.cebe@fxinnovation.com>
2021-12-03 10:07:12 -0500

    fix: fxTerraformWithAccessToken.groovy

--- vars/fxTerraformWithAccessToken.groovy
+++ vars/fxTerraformWithAccessToken.groovy
@@ -5,8 +5,12 @@ def call(Map config = [:], Map closures = [:]) {
 
   mapAttributeCheck(config, 'testEnvironmentCredentialId', CharSequence, '', '"testEnvironmentCredentialId" parameter is mandatory.')
   mapAttributeCheck(config, 'publishEnvironmentCredentialId', CharSequence, config.testEnvironmentCredentialId)
+  mapAttributeCheck(config, 'testEnvironmentTokenId', CharSequence, '', '"testEnvironmentTokenId" parameter is mandatory.')
+  mapAttributeCheck(config, 'publishEnvironmentTokenId', CharSequence, config.testEnvironmentTokenId)
   mapAttributeCheck(config, 'providerOwnerVariableName', CharSequence, 'owner')
   mapAttributeCheck(config, 'providerAccessTokenVariableName', CharSequence, 'token')
+  mapAttributeCheck(config, 'providerUsernameVariableName', CharSequence, 'access_key')
+  mapAttributeCheck(config, 'providerPasswordVariableName', CharSequence, 'secret_key')
   mapAttributeCheck(config, 'testPlanVars', List, [])
   mapAttributeCheck(config, 'publishPlanVars', List, [])
   mapAttributeCheck(config, 'validateVars', List, [])
@@ -16,16 +20,28 @@ def call(Map config = [:], Map closures = [:]) {
   withCredentials([
     usernamePassword(
       credentialsId: config.testEnvironmentCredentialId,
+      usernameVariable: 'TF_username_test',
+      passwordVariable: 'TF_password_test'
+    ),
+    usernamePassword(
+      credentialsId: config.publishEnvironmentCredentialId,
+      usernameVariable: 'TF_username_publish',
+      passwordVariable: 'TF_password_publish'
+    ),
+    usernamePassword(
+      credentialsId: config.testEnvironmentTokenId,
       usernameVariable: 'TF_owner_test',
       passwordVariable: 'TF_token_test'
     ),
     usernamePassword(
-      credentialsId: config.publishEnvironmentCredentialId,
+      credentialsId: config.publishEnvironmentTokenId,
       usernameVariable: 'TF_owner_publish',
       passwordVariable: 'TF_token_publish'
     ),
   ]) {
     config.testPlanVars = [
+      "${config.providerUsernameVariableName}=${TF_username_test}",
+      "${config.providerPasswordVariableName}=${TF_password_test}",
       "${config.providerOwnerVariableName}=${TF_owner_test}",
       "${config.providerAccessTokenVariableName}=${TF_token_test}",
     ] + config.testPlanVars
@@ -33,14 +49,19 @@ def call(Map config = [:], Map closures = [:]) {
     config.validateVars = [
       "${config.providerOwnerVariableName}=${TF_owner_test}",
       "${config.providerAccessTokenVariableName}=${TF_token_test}",
+      "${config.providerUsernameVariableName}=${TF_username_test}",
+      "${config.providerPasswordVariableName}=${TF_password_test}",
     ] + config.validateVars
 
     config.publishPlanVars = [
       "${config.providerOwnerVariableName}=${TF_owner_publish}",
       "${config.providerAccessTokenVariableName}=${TF_token_publish}",
+      "${config.providerUsernameVariableName}=${TF_username_publish}",
+      "${config.providerPasswordVariableName}=${TF_password_publish}",
     ] + config.publishPlanVars
 
-    config.inspecPassword = TF_token_test
+    config.inspecUsername = TF_username_test
+    config.inspecPassword = TF_password_test
 
    fxTerraform(config, closureHelper.getClosures())
   }
