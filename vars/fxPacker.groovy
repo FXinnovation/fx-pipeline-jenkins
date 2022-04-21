27152f3f59bee71bdeff914ebdc02987e3ef9bd1
Kim Samba <kim.samba@fxinnovation.com>
2022-04-21 13:24:41 -0400

    feat: add variable for skipAmi and remove publish

--- vars/fxPacker.groovy
+++ vars/fxPacker.groovy
@@ -18,6 +18,7 @@ def call(Map config = [:]){
         ){
           def packerConfiguration = [
             commandTarget: 'configuration.pkr.hcl',
+            vars: ["skip_create_ami=${!scmInfo.isPublishable()}"],
             dockerEnvironmentVariables: [
               AWS_ACCESS_KEY_ID:     client_id,
               AWS_SECRET_ACCESS_KEY: client_secret,
@@ -28,7 +29,6 @@ def call(Map config = [:]){
             [
               validateConfig: [packerConfiguration],
               buildConfig:    [packerConfiguration],
-              publish:        true
             ]
           )
         }
